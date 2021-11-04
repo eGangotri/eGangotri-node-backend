e25cd67ba4a5fc9c7173d640c7772e5b74870a56
eGangotri <egangotritrust@gmail.com>
2021-11-05 02:36:33 +0800

    renamings

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', function (req, res) {
-  res.send('eGangotri-node-upload-monitor');
+  res.send('eGangotri-node-node-backend');
 })
 
 app.use('/itemsQueued', itemsQueuedRoute);
