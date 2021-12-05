a15bfd472f27a7adb15f617aef1d2661a95bcd5e
eGangotri Trust <egangotritrust@gmail.com>
2021-12-06 04:56:40 +0530

    .

--- src/imgToPdf/tifToPdf.ts
+++ src/imgToPdf/tifToPdf.ts
@@ -1,39 +1,47 @@
 import * as fs from 'fs';
 import { PDFDocument } from 'pdf-lib';
-import { tiffToPng, getAllTifs, deleteAllPngs } from './imgUtils';
+import { tiffToPng, getAllTifs, deleteAllPngs, getAllPngs } from './imgUtils';
 import * as path from 'path';
+import { createPngs } from './tifToPng';
 
-async function createPdf(pdfName: string, directoryPath: string) {
-    const pdfDoc = await PDFDocument.create()
-    const _pngs = tiffToPng(getAllTifs(directoryPath),
-        directoryPath);
+async function createPdf2(directoryPath: string) {
+    const _pngs = await getAllPngs(directoryPath);
+    const pdfName = path.parse(directoryPath).name + ".pdf";
+    console.log(`pdfName ${pdfName}`)
+   // await imagesToPdf(_pngs, pdfName)
+}
+async function createPdf(directoryPath: string) {
+    const _pngs = await getAllPngs(directoryPath);
+
     console.log(`_tiffToPng ${_pngs}`);
+    const pdfDoc = await PDFDocument.create()
 
-    _pngs.forEach(async (_png) => {
+    for (let _png of _pngs) {
         const pngImageBytes = fs.readFileSync(_png);
         const pngImage = await pdfDoc.embedPng(pngImageBytes)
         const pngDims = pngImage.scale(1)
 
         const page = pdfDoc.addPage()
-
-        page.drawImage(pngImage, {
-            x: 0,
-            y: 0,
-            width: pngDims.width,
-            height: pngDims.height,
-        })
+        page.drawImage(pngImage)
         const pdfBytes = await pdfDoc.save()
-        const pdfName = path.parse(directoryPath).name + ".pdf";     //=> "hello"
+        const pdfName = path.parse(directoryPath).name + ".pdf";
         console.log(`pdfName ${pdfName}`)
         fs.writeFileSync(`${directoryPath}\\${pdfName}`, pdfBytes);
-    })
+    }
 }
 async function createPdfAndDeleteGeneratedFiles(directoryPath: string) {
-     createPdf('${directoryPath}\\sample.pdf', directoryPath).then(() =>{
-         console.log("after pdf creation. Now delete all generated .pngs")
-         //deleteAllPngs(directoryPath);
-     })
+    await createPdf2(directoryPath);
+    console.log("after pdf creation. Now delete all generated .pngs")
+    //deleteAllPngs(directoryPath);
 }
 
-createPdfAndDeleteGeneratedFiles("C:\\tmp\\1")
+
+
+(async () => {
+    const src = "C:\\tmp\\1"
+    await deleteAllPngs(src);
+    await createPngs(src)
+    console.log(`createPngs ends`)
+    await createPdfAndDeleteGeneratedFiles(src);
+})()
 
