3dbfaf6199173ba19879c68624d4018c3489e17a
eGangotri Trust <egangotritrust@gmail.com>
2021-12-07 20:35:21 +0530

    Intro PAge

--- src/imgToPdf/utils/PdfUtils.ts
+++ src/imgToPdf/utils/PdfUtils.ts
@@ -8,20 +8,34 @@ const PDFMerger = require('pdf-merger-js');
 const PDFDocument = require('pdfkit');
 
 const FOOTER_TEXT = 'CC-0. Kavikulguru Kalidas Sanskrit University Ramtek Collection';
-const FOOTER_FONT = 'Times-Roman';
+const PDF_FONT = 'Times-Roman';
 const FOOTER_LINK = 'https://kksu.org/';
-const INTRO_PDF = 'E:\\KKSU.pdf';
+const INTRO_BANNER = 'E:\\KKSU_Banner.jpg';
+const ADD_INTRO_PDF = true;
+const INTRO_TEXT = `This PDF you are browsing now is a digitized copy of Rare books and manuscripts from the Jnanayogi Dr. Shrikant Jichkar Knowledge Resource Center Library located in Kavikula Guru Kalidas Sanskrit University Ramtek, Maharashtra.
+
+
+About KKSU
+The University was established on 18 September 1997. It is named to pay rich tribute to Legendary Sanskrit Mahakavi Kalidas. KKSU is an institution dedicated to the advanced learning of Sanskrit. It is located at Ramtek which falls under Nagpur District, the second capital of Maharashtra. KKSU is the First Sanskrit University of Maharashtra.
+The pdf is offered to the Community of Scholars free of charge without restrictions.
+
+Website
+https://kksu.co.in/`
 
 //https://pdfkit.org/docs/text.html
-export async function createPdf(src:string, dest: string) {
-    const _pngs = await getAllPngs(pngFolderName(src,dest));
+export async function createPdf(src: string, dest: string) {
+    const _pngs = await getAllPngs(pngFolderName(src, dest));
     const pdf = dest + "\\" + path.parse(src).name + ".pdf";
-    console.log(`pdfName ${pdf}`)
     const doc = new PDFDocument({ autoFirstPage: false, bufferPages: true });
     doc.pipe(fs.createWriteStream(pdf)); // write to PDF
 
+    let introPDFAdded = false
     for (let png of _pngs) {
         let img = doc.openImage(png);
+        if (!introPDFAdded && ADD_INTRO_PDF) {
+            introPDFAdded = true;
+            addBanner(doc, img);
+        }
         doc.addPage({ size: [img.width, img.height] });
         doc.image(img, 0, 0)
         addFooter(doc)
@@ -29,23 +43,36 @@ export async function createPdf(src:string, dest: string) {
 
     // finalize the PDF and end the stream
     doc.end();
-    console.log(`${pdf} created`)
-    if(checkPageCountAndLogReport(doc, pdf, _pngs.length)){
-        //await mergepPdfs(pdf)
-    }
+    console.log(`Created \n\t${pdf}/`)
+    checkPageCountAndLogReport(doc, pdf, _pngs.length);
 }
 
-function calculateFontSize(pageHeight:number){
-    const fontSize =  pageHeight*0.02 > 12 ? pageHeight*0.02: 12;
+function addBanner(doc: any, img: any) {
+    doc.addPage({ size: [img.width, img.height] });
+    const banner = doc.openImage(INTRO_BANNER)
+    doc.image(banner, doc.page.margins.left, doc.page.margins.top, 
+        { width: img.width - ( doc.page.margins.left+doc.page.margins.right) })
+        doc.moveDown();
+
+    doc.font(PDF_FONT).fontSize(calculateFontSize(img.height))
+        .fillColor('black')
+        .text(INTRO_TEXT, doc.page.margins.left, banner.height + doc.page.margins.top + (img.height * 0.2), {
+            align: 'left'
+        });
+    addFooter(doc);
+}
+
+function calculateFontSize(pageHeight: number) {
+    const fontSize = pageHeight * 0.02 > 12 ? pageHeight * 0.02 : 12;
     return fontSize;
 }
 
-function addFooter(doc:any){
+function addFooter(doc: any) {
     let oldBottomMargin = doc.page.margins.bottom;
     doc.page.margins.bottom = 0 //Dumb: Have to remove bottom margin in order to write into it
     const pageHeight = doc.page.height
     const yCoordinate = pageHeight * 0.95
-    doc.font(FOOTER_FONT).fontSize(calculateFontSize(pageHeight))
+    doc.font(PDF_FONT).fontSize(calculateFontSize(pageHeight))
         .fillColor('black')
         .text(FOOTER_TEXT, 0, yCoordinate, {
             link: FOOTER_LINK,
@@ -54,28 +81,28 @@ function addFooter(doc:any){
     doc.page.margins.bottom = oldBottomMargin; // ReProtect bottom margin
 }
 
-function checkPageCountAndLogReport(doc: any,pdf:string,  pngCount:number) {
+function checkPageCountAndLogReport(doc: any, pdf: string, pngCount: number) {
     const range = doc.bufferedPageRange();
-    if (range.start === pngCount) {
-        GENERATION_REPORT.push(`${pdf}(${pngCount}) created with PageCount same as png count`)
+    if (range.start === pngCount + (ADD_INTRO_PDF ? 1 : 0)) {
+        GENERATION_REPORT.push(`${pdf}(${pngCount + (ADD_INTRO_PDF ? 1 : 0)}) created with PageCount same as png count`)
     }
     else {
-        GENERATION_REPORT.push(`***Error${pdf}(${pngCount}) created with PageCount wrong!!!`)
+        GENERATION_REPORT.push(`***Error${pdf}(${pngCount + (ADD_INTRO_PDF ? 1 : 0)}) created with PageCount wrong!!!`)
     }
     console.log(GENERATION_REPORT)
     return range.start === pngCount
 }
 
-export async function createPdfAndDeleteGeneratedFiles(src:string,dest: string) {
+export async function createPdfAndDeleteGeneratedFiles(src: string, dest: string) {
     await createPdf(src, dest);
     console.log("after pdf creation. Now delete all generated .pngs")
-    await deleteAllPngs(pngFolderName(src,dest));
+    await deleteAllPngs(pngFolderName(src, dest));
 }
 
-export async function mergepPdfs(pdf:string){
- const merger = new PDFMerger();
- console.log(`merging ${INTRO_PDF}`)
-  merger.add(INTRO_PDF); 
-  merger.add(pdf); 
-  await merger.save(pdf + "x.pdf"); //save under given name and reset the internal document
+export async function mergepPdfs(pdf: string, INTRO_PDF: string) {
+    const merger = new PDFMerger();
+    console.log(`merging ${INTRO_PDF}`)
+    merger.add(INTRO_PDF);
+    merger.add(pdf);
+    await merger.save(pdf + "x.pdf"); //save under given name and reset the internal document
 }
