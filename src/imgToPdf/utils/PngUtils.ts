e90c26ab32fc1c725cbec8c86e24ee38b4c04692
eGangotri Trust <egangotritrust@gmail.com>
2021-12-07 09:14:27 +0530

    extra PDF

--- src/imgToPdf/utils/PngUtils.ts
+++ src/imgToPdf/utils/PngUtils.ts
@@ -1,12 +1,25 @@
-import { getAllTifs } from "./ImgUtils";
+import { getAllPngs, getAllTifs } from "./ImgUtils";
 import * as path from 'path';
+import * as fs from 'fs';
+
 const sharp = require("sharp")
 
-export async function createPngs(src:string, dest:string){
-    const tiffs = await getAllTifs(src);
-    for(let tiff of tiffs){
-        await tiffToPng(tiff,dest)
+export function pngFolderName(src:string, dest:string){
+    const folderName = dest + "\\" + path.parse(src).name
+    if (!fs.existsSync(folderName)){
+        fs.mkdirSync(folderName);
     }
+    return folderName;
+}
+export async function tifftoPngs(src:string, dest:string){
+    const tiffs = await getAllTifs(src);
+    const folderForPngs = pngFolderName(src,dest);
+
+    for(let tiff of tiffs){
+        await tiffToPng(tiff,folderForPngs)
+    }
+    const pngCount = (await getAllPngs(folderForPngs)).length
+    return {tiffsCount: tiffs.length, pngCount, countMatch: tiffs.length == pngCount };
 }
 
 async function tiffToPng(tifFile:string, dest:string){
