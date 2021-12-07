1f9d49cd692f7ca96df609e501db3bed92ec69dd
eGangotri Trust <egangotritrust@gmail.com>
2021-12-08 03:19:02 +0530

    improved the Intro Page

--- src/imgToPdf/utils/Utils.ts
+++ src/imgToPdf/utils/Utils.ts
@@ -7,8 +7,47 @@ export const getDirectories = source =>
 
 export function formatTime(timeLapseinMS:number){
      const timeLapseInSecs = timeLapseinMS/1000
+     let timeLapse = `${(timeLapseInSecs*100)/100} sec(s)`
      if(timeLapseInSecs > 60){
-          return `${timeLapseInSecs/60} minute(s)`
+          timeLapse = `${((timeLapseInSecs/60)*100)/100} minute(s)`
      }
-     return `${timeLapseInSecs} sec(s)` 
+     return  timeLapse
 }
+
+export async function folderCountEqualsPDFCount(srcFolderCount:number,dest:string){
+     const pdfCount = (await getAllPdfs(dest)).length
+     const folderCountCheck = srcFolderCount === pdfCount
+     if(folderCountCheck){
+          return `Exactly ${srcFolderCount} pdfs were created from same number of Folders.`
+     }
+     else{
+          return `****Error. Folder Count ${srcFolderCount} and no. of pdfs ${pdfCount} generated is different by (${srcFolderCount-pdfCount}).`
+     }
+}
+
+export async function deleteFiles(files: Array<string>) {
+     for (let file of files) {
+         try {
+             fs.unlinkSync(file);
+         } catch (err) {
+             console.error(err)
+         }
+     };
+ }
+ 
+ export const getAllPdfs = async (dir: string) => {
+     return await getAllFilesOfGivenType(dir, [".pdf"]);
+ }
+ export const getAllFilesOfGivenType = async (dir: string, _types: Array<string> = []) => {
+     let files = []
+     const contentList = fs.readdirSync(dir)
+     for (let content of contentList) {
+         if (content.toLowerCase().endsWith(_types[0]) || (_types.length > 1 && content.toLowerCase().endsWith(_types[1]))) {
+             files.push(dir + "\\" + content);
+         }
+     }
+     console.log(`Found ${files.length} ${_types[0]}(s) of ${contentList.length}  items in \n\t${dir}`)
+ 
+     return files;
+ }
+ 
