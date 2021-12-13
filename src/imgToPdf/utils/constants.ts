2054a76e568b226123f58f0fbd43e8e00c087e62
eGangotri Trust <egangotritrust@gmail.com>
2021-12-14 02:46:54 +0530

    streamlined

--- src/imgToPdf/utils/constants.ts
+++ src/imgToPdf/utils/constants.ts
@@ -1,30 +1,5 @@
 
-export const FOOTER_TEXT = 'CC-0. In Public Domain.Kavikulguru Kalidas Sanskrit University Ramtek Collection';
-export const PDF_FONT = 'Times-Roman';
-export const FOOTER_LINK = 'https://kksu.org/';
-export const INTRO_BANNER = 'E:\\KKSU_Banner.jpg';
 export const PNG_SUB_FOLDER = "\\pngs\\"
 export const PDF_SUB_FOLDER = "\\pdfs\\"
 export const PDF_EXT = ".pdf"
 
-export const INTRO_TEXT = `This PDF you are browsing now is a digitized copy of rare books and manuscripts from the Jnanayogi Dr. Shrikant Jichkar Knowledge Resource Center Library located in Kavikulaguru Kalidas Sanskrit University Ramtek, Maharashtra.
-
-Digitization was executed by NMM(https://www.namami.gov.in/)
-
-About KKSU
-The University was established on 18 September 1997. KKSU is an institution dedicated to the advanced learning of Sanskrit. It is located at Ramtek in Nagpur District, Maharashtra.
-The pdf is offered freely to the Community of Scholars with the intent to promote Sanskrit Learning.
-
-Website
-https://kksu.co.in/
-
-Sincerely,
-
-Prof. Shrinivasa Varkhedi
-Hon'ble Vice-Chancellor
-
-Dr. Deepak Kapade
-Librarian
-
-Digital Uploaded by eGangotri Digital Preservation Trust, New Delhi
-https://egangotri.wordpress.com/`
