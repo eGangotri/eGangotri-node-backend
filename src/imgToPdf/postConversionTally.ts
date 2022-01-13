097a0bccf7af5300d1948d3d72b7ae001a8b5d35
eGangotri <egangotritrust@gmail.com>
2022-01-13 22:42:00 +0800

    post conversion tally

--- src/imgToPdf/postConversionTally.ts
+++ src/imgToPdf/postConversionTally.ts
@@ -1,8 +1,34 @@
-import { getDirectories, getDirectoriesWithFullPath } from "./utils/Utils";
+import { getAllPdfs, getDirectories, getDirectoriesWithFullPath } from "./utils/Utils";
 
-const _dir = getDirectoriesWithFullPath("E:\\August-2019_reduced");
+async function tally(dirToTally: string) {
+  let tallyFailureCount = 0;
+  let tailySuccessCount = 0; 
+  const folders = await getDirectoriesWithFullPath(dirToTally);
+  let tallyStats = folders.map(async (folder) => {
+    const tokens = folder.split("(");
+    let folderLengthFromTitle = -1
+    if (tokens && tokens.length === 2) {
+      folderLengthFromTitle = parseInt(tokens[1]);
+      console.log(folderLengthFromTitle);
+    }
+    const pdfCount = (await getAllPdfs(folder)).length;
+    const tallyCheck = folderLengthFromTitle  == pdfCount
+    if(tallyCheck){
+        tailySuccessCount++
+    }
+    else{
+        tallyFailureCount++
+    }
+    console.log(`Tally Check for ${folder} ${tallyCheck}`);
+    return {
+        folder,
+        tallyCheck
+    }
+  });
 
-//ramtek-1_01-08-2019(19)
-_dir.array.forEach(element => {
-    
-});
+  console.log(await Promise.all(tallyStats));
+  console.log(`Taily Failure: ${tallyFailureCount}`);
+  console.log(`Taily Success Count: ${tailySuccessCount}`);
+}
+
+tally("C:\\tmp\\tallyTest");
