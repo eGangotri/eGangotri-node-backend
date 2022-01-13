377b167b8253d194e00214f8a1775543858966bf
eGangotri Trust <egangotritrust@gmail.com>
2022-01-13 19:44:29 +0530

    adding postConversionTally

---
+++ src/imgToPdf/postConversionTally.ts
@@ -0,0 +1,8 @@
+import { getDirectories, getDirectoriesWithFullPath } from "./utils/Utils";
+
+const _dir = getDirectoriesWithFullPath("E:\\August-2019_reduced");
+
+//ramtek-1_01-08-2019(19)
+_dir.array.forEach(element => {
+    
+});
