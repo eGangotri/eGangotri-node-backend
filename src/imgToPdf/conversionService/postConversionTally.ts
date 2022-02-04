d1d49c1f644934ceffe8f9b8f942a756d1bec514
eGangotri Trust <egangotritrust@gmail.com>
2022-02-04 05:39:15 +0530

    tally improved

--- src/imgToPdf/conversionService/postConversionTally.ts
+++ src/imgToPdf/conversionService/postConversionTally.ts
@@ -2,23 +2,19 @@ import { getAllFilesOfGivenType, getAllPdfs, getDirectories, getDirectoriesWithF
 
 const TALLY_FOR_FOLDERS = 1;
 const TALLY_FOR_PDFS = 2;
-let  TOTAL_NUMBER_OF_FOLDERS_HAVING_PDF_CREATIBLES = 0
-let  TOTAL_NUMBER_OF_TARGETS_GENERATED = 0
-let FOLDER_COUNT_EQUALS_GENERATED_FOLDERS = false
-async function tally(dirToTally: string, tallyType:number = 1) {
+let TOTAL_NUMBER_OF_FOLDERS_HAVING_PDF_CREATIBLES = 0
+let TOTAL_NUMBER_OF_TARGETS_GENERATED = 0
+let FOLDER_COUNT_EQUALS_GENERATED_FOLDERS_OR_PDFS = false;
+
+async function tally(dirToTally: string, tallyType: number = 1) {
   let tallyFailureCount = 0;
-  let tallySuccessCount = 0; 
+  let tallySuccessCount = 0;
   const folders = await getDirectoriesWithFullPath(dirToTally);
 
   let dirToTallySubFolderCountFromTitle = parseInt(dirToTally.split("(")[1]);
-  if(dirToTallySubFolderCountFromTitle !== folders.length){
-    console.log(`Folder Count ${folders.length} not same as ${dirToTallySubFolderCountFromTitle}`);
-  }
-  else{
-    FOLDER_COUNT_EQUALS_GENERATED_FOLDERS = true
-  }
+ 
 
-  let totalTallyableDirectoryCount = folders.length; 
+  let totalTallyableDirectoryCount = folders.length;
   let tallyStats = folders.map(async (folder) => {
     const tokens = folder.split("(");
     let folderLengthFromTitle = -1
@@ -27,42 +23,49 @@ async function tally(dirToTally: string, tallyType:number = 1) {
       console.log(`folderLengthFromTitle ${folderLengthFromTitle} from ${folder}`);
     }
     let targetCount = 0;
-    if(tallyType === 1){
+    if (tallyType === 1) {
       targetCount = (await getDirectoriesWithFullPath(folder)).length;
     }
-    else{
+    else {
       targetCount = (await getAllPdfs(folder)).length;
     }
-    TOTAL_NUMBER_OF_TARGETS_GENERATED+=targetCount
+    TOTAL_NUMBER_OF_TARGETS_GENERATED += targetCount
 
     console.log(`Item Count ${targetCount} for Target Item ${folder}`);
-    TOTAL_NUMBER_OF_FOLDERS_HAVING_PDF_CREATIBLES+= folderLengthFromTitle
+    TOTAL_NUMBER_OF_FOLDERS_HAVING_PDF_CREATIBLES += folderLengthFromTitle
     const tallyCheck = folderLengthFromTitle === targetCount
-    if(tallyCheck){
-        tallySuccessCount++
+    if (tallyCheck) {
+      tallySuccessCount++
     }
-    else{
-        tallyFailureCount++
+    else {
+      tallyFailureCount++
     }
     console.log(`Tally Check for ${folder} ${tallyCheck}`);
     return {
-        folder,
-        tallyCheck,
-        diff: folderLengthFromTitle - targetCount 
+      folder,
+      tallyCheck,
+      diff: folderLengthFromTitle - targetCount
     }
   });
 
-  const _tallType  = tallyType===TALLY_FOR_FOLDERS?"Folder Tally":"PDF Tally"
+
+  if (dirToTallySubFolderCountFromTitle !== folders.length) {
+    console.log(`Folder Count ${folders.length} not same as ${dirToTallySubFolderCountFromTitle}`);
+  }
+  else {
+    FOLDER_COUNT_EQUALS_GENERATED_FOLDERS_OR_PDFS = true
+  }
+  const _tallType = tallyType === TALLY_FOR_FOLDERS ? "Folder Tally" : "PDF Tally"
   console.log(await Promise.all(tallyStats));
   console.log(`TOTAL_NUMBER_OF_FOLDERS_HAVING_PDF_CREATIBLES: ${TOTAL_NUMBER_OF_FOLDERS_HAVING_PDF_CREATIBLES}`)
   console.log(`TOTAL_NUMBER_OF_TARGETS_GENERATED: ${TOTAL_NUMBER_OF_TARGETS_GENERATED}`)
-  const targetItemCountDiff = TOTAL_NUMBER_OF_FOLDERS_HAVING_PDF_CREATIBLES-TOTAL_NUMBER_OF_TARGETS_GENERATED
-  console.log(`Target Item Creation Count(${TOTAL_NUMBER_OF_FOLDERS_HAVING_PDF_CREATIBLES}==${TOTAL_NUMBER_OF_TARGETS_GENERATED})`,targetItemCountDiff=== 0? "Matches. Sucesss": `Short by ${targetItemCountDiff}. Failed`)
-  
+  const targetItemCountDiff = TOTAL_NUMBER_OF_FOLDERS_HAVING_PDF_CREATIBLES - TOTAL_NUMBER_OF_TARGETS_GENERATED
+  console.log(`Target Item Creation Count(${TOTAL_NUMBER_OF_FOLDERS_HAVING_PDF_CREATIBLES}==${TOTAL_NUMBER_OF_TARGETS_GENERATED})`, targetItemCountDiff === 0 ? "Matches. Sucesss" : `Short by ${targetItemCountDiff}. Failed`)
+
   console.log(`\n${_tallType} Failure(s): ${tallyFailureCount}`);
   console.log(`${_tallType} Success Count: ${tallySuccessCount}`);
-  console.log(`${_tallType} Is Success Count(${tallySuccessCount}) and No. of Target Items(${totalTallyableDirectoryCount}) matching ${totalTallyableDirectoryCount=== tallySuccessCount?  "Yes Complete Success" : "!!!! FAILURES !!!!"}`)
-  console.log(`FOLDER_COUNT_EQUALS_GENERATED_FOLDERS: ${FOLDER_COUNT_EQUALS_GENERATED_FOLDERS}`);
+  console.log(`${_tallType} Is Success Count(${tallySuccessCount}) and No. of Target Items(${totalTallyableDirectoryCount}) matching ${totalTallyableDirectoryCount === tallySuccessCount ? "Yes Complete Success" : "!!!! FAILURES !!!!"}`)
+  console.log(`FOLDER_COUNT_EQUALS_GENERATED_FOLDERS_OR_PDFS: ${FOLDER_COUNT_EQUALS_GENERATED_FOLDERS_OR_PDFS}`);
 }
 
 //Before Merge
