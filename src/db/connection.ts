f1712bc7f74424d5957ebaeceef88f69579edc7f
Chetan Pandey <chetan.pandey@biqmind.com>
2022-04-20 14:27:19 +0800

    DB changes

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,7 +1,11 @@
+const production = true;
 const DB_NAME = 'archive_upload_monitor';
-const HOST = 'localhost';
-const PORT = 27017;
-const DB_URL =  `mongodb://${HOST}:${PORT}/${DB_NAME}`;
+const HOST = production?'egangotri:bcWbOF0rPGAU4BhJJdlGjVTdbs1xAboMl9poX68Qm4UOMusvdGds4JNxRPTb3RzqLaXoAGEI80H5P49dIfnmcg==@egangotri.mongo.cosmos.azure.com:':'localhost';
+const PORT = production?'10255':27017;
+
+const DB_URL = production?
+'mongodb://egangotri:bcWbOF0rPGAU4BhJJdlGjVTdbs1xAboMl9poX68Qm4UOMusvdGds4JNxRPTb3RzqLaXoAGEI80H5P49dIfnmcg==@egangotri.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@egangotri@'
+:`mongodb://${HOST}:${PORT}/${DB_NAME}`;
 
 export const connection_config = {
     HOST,
