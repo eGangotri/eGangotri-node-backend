718a8c3fb90f6bb5f137f9ae6b2ca2b3d65e4a04
eGangotri <egangotritrust@gmail.com>
2023-05-28 10:29:45 +0530

    netlify

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,6 +1,6 @@
 const configData = require("../config.json");
-const MONGO_CONFIGS = require("../pwd.json");
-
+//const MONGO_CONFIGS = require("../pwd.json");
+const MONGO_CONFIGS = process.env
 export const MONGO_DB_URL = `${MONGO_CONFIGS.MONGO_DB_PROTOCOL}${MONGO_CONFIGS.MONGO_ATLAS_USER}:${MONGO_CONFIGS.MONGO_ATLAS_PWD}@${MONGO_CONFIGS.MONGO_DB_PATH}`;
 
 export const MONGO_OPTIONS = {
