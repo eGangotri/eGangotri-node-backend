521c4313a0eb79f8b76f83147430c34ebc6ae07d
eGangotri <egangotritrust@gmail.com>
2023-05-31 20:24:22 +0530

    filters introduced

---
+++ src/services/constants.ts
@@ -0,0 +1,83 @@
+
+export const CSV_HEADER = [
+    {
+      key: "dateOfReport",
+      label: "DATE",
+    },
+    {
+      key: "operatorName",
+      label: "NAME",
+    },
+    {
+      key: "center",
+      label: "Center",
+    },
+    {
+      key: "lib",
+      label: "Lib",
+    },
+    {
+      key: "totalPdfCount",
+      label: "TOTAL PDF COUNT",
+    },
+    {
+      key: "totalPageCount",
+      label: "TOTAL PAGE COUNT(Pages)",
+    },
+    {
+      key: "totalSize",
+      label: "TOTAL SIZE",
+    },
+  ];
+  
+  export const CSV_HEADER_API2 = 
+  [
+    {
+      id: "dateOfReport",
+      title: "Date of Report",
+    },
+    {
+      id: "operatorName",
+      title: "Operator Name",
+    },
+    {
+      id: "center",
+      title: "Center",
+    },
+    {
+      id: "lib",
+      title: "Lib",
+    },
+    {
+      id: "totalPdfCount",
+      title: "TOTAL PDF COUNT",
+    },
+    {
+      id: "totalPageCount",
+      title: "TOTAL PAGE COUNT(Pages)",
+    },
+    {
+      id: "totalSize",
+      title: "TOTAL SIZE",
+    }
+  ]
+  
+  export const dailyDetailReportHeader = [
+    {
+      key: "operatorName",
+      label: "Operator Name",
+    },
+    {
+      key: "fileName",
+      label: "File Name",
+    },
+    {
+      key: "pageCount",
+      label: "PAGE COUNT(Pages)",
+    },
+    {
+      key: "fileSize",
+      label: "File Size",
+    },
+  ];
+  
