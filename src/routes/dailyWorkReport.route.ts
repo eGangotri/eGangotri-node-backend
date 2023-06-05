d88a5f3aa1fd51d56582f401ae9ae2c3fc803c27
eGangotri <egangotritrust@gmail.com>
2023-06-05 17:26:10 +0530

    Daily Work report re-submitted within 2 hours will be overwritten

--- src/routes/dailyWorkReport.route.ts
+++ src/routes/dailyWorkReport.route.ts
@@ -1,8 +1,10 @@
 const express = require("express");
 import { DailyWorkReport } from "../models/dailyWorkReport";
-import { generateCSV, generateCSVApi2 as generateCSVAsJsonArray, generateCSVAsFile, getListOfDailyWorkReport } from "../services/dailyWorkReportService";
+import { generateCSV, generateCSVApi2 } from "../services/CsvUtil";
+import { generateCSVAsFile, getListOfDailyWorkReport } from "../services/dailyWorkReportService";
 import { Request, Response } from "express";
 import { validateUserFromRequest } from "./utils";
+import { getDateTwoHoursBeforeNow } from "../services/Util";
 
 export const dailyWorkReportRoute = express.Router();
 
@@ -11,32 +13,32 @@ export const dailyWorkReportRoute = express.Router();
 POST http://localhost/dailyWorkReport/add 
 JSON Body 
 {
-	"operatorName": "test2",
-	"center": "Haridwar",
-	"lib": "Gurukul-Kangri",
-	"totalPdfCount": 3,
-	"totalPageCount": 896,
-	"totalSize": "460.3 MB",
-	"totalSizeRaw": "3333333333",
-	"dateOfReport": "2023-05-30T17:11:57.792Z",
-	"pageCountStats": [
-		{
-			"fileName": "The complete works of the swami vivekananda).pdf",
-			"pageCount": 471,
-			"fileSize": "234.58 MB"
-		},
-		{
-			"fileName": "Sharika leela.pdf",
-			"pageCount": 284,
-			"fileSize": "111.25 MB"
-		},
-		{
-			"fileName": "The temples of india).pdf",
-			"pageCount": 141,
-			"fileSize": "114.47 MB"
-		}
-	],
-	"password": "22222"
+  "operatorName": "test2",
+  "center": "Haridwar",
+  "lib": "Gurukul-Kangri",
+  "totalPdfCount": 3,
+  "totalPageCount": 896,
+  "totalSize": "460.3 MB",
+  "totalSizeRaw": "3333333333",
+  "dateOfReport": "2023-05-30T17:11:57.792Z",
+  "pageCountStats": [
+    {
+      "fileName": "The complete works of the swami vivekananda).pdf",
+      "pageCount": 471,
+      "fileSize": "234.58 MB"
+    },
+    {
+      "fileName": "Sharika leela.pdf",
+      "pageCount": 284,
+      "fileSize": "111.25 MB"
+    },
+    {
+      "fileName": "The temples of india).pdf",
+      "pageCount": 141,
+      "fileSize": "114.47 MB"
+    }
+  ],
+  "password": "22222"
 }
  */
 
@@ -48,23 +50,28 @@ dailyWorkReportRoute.post("/add", async (req: Request, resp: Response) => {
       const wr = new DailyWorkReport(req.body);
 
       //Check if any request was sent in Last 2 hours
-      const _query:typeof req.query = {};
+      const _query: typeof req.query = {};
       _query['isLastTwoHours'] = 'true';
       _query['operatorName'] = operatorName;
 
-      const items = await getListOfDailyWorkReport(_query);
-      if (items && items.length > 0) {
-        console.log(`dailyWorkReportRoute: /add ${JSON.stringify(items)} ${items[0]}`);
-         const updatedDocument = await wr.updateOne({_id:items[0]._id}).exec();
-
-         console.log(`dailyWorkReportRoute:updatedDocument ${JSON.stringify(updatedDocument)}}`);
-
+      const preExisting = await getListOfDailyWorkReport(_query);
+      if (preExisting && preExisting.length >= 1) {
+        console.log(` preExisting[0]._id . ${preExisting.length}`);
+        const filter = {
+          _id: { $in: preExisting.map(x => x._id) },
+          //just to be extra safe
+          createdAt: {
+            $gte: new Date(getDateTwoHoursBeforeNow()),
+            $lte: new Date(new Date()),
+          },
+        };
+        await DailyWorkReport.deleteMany(filter);
+        await wr.save();
         resp.status(200).send({
-          "warning": `Since Last Submission Request < 2 Hours. exisiting Data is merely overwritten not inserted.${wr._id} for ${operatorName}`
+          "warning": `Since Last Submission Request < 2 Hours. exisiting Data is merely overwritten not inserted. for ${operatorName}`
         });
 
       }
-
       else {
         console.log(`dailyWorkReportRoute /add ${JSON.stringify(wr)}`);
         await wr.save();
@@ -119,7 +126,7 @@ dailyWorkReportRoute.get("/csvAsJsonArray", async (req: Request, resp: Response)
     console.log(
       `after getListOfDailyWorkReport retrieved item count: ${items[0]}`
     );
-    const _csv = generateCSVAsJsonArray(items)
+    const _csv = generateCSVApi2(items)
     resp.status(200).send(_csv);
   } catch (err: any) {
     console.log("Error", err);
