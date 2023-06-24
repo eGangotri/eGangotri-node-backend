cccff4251622f002037b1533998246a885770e8d
egangotri <egangotritrust@gmail.com>
2023-06-25 03:48:31 +0800

    vanity

--- src/vanityService/VanityPdf.ts
+++ src/vanityService/VanityPdf.ts
@@ -5,6 +5,7 @@ import { getAllPdfsInFolders, mkDirIfDoesntExists } from "../imgToPdf/utils/Util
 import { prepareDocument } from "../imgToPdf/utils/PdfUtils";
 const path = require('path');
 import PDFKit from 'pdfkit';
+import { MAX_IMG_WIDTH, _pdfRoot, imgFile, introText } from "./constants";
 
 /** This should be dynamic based on the Width/height */
 const FONT_SIZE = 16
@@ -55,15 +56,24 @@ const createIntroPageWithImage = async (imagePath: string, pdfToVanitize: string
     return `${_introPath}\\${introPDfName}`
 }
 
-export const addImageToFirstPage = async (doc: any, pathToImg: string, width: number, height: number) => {
+export const addImageToFirstPage = async (doc: any, pathToImg: string, imgWidth: number, height: number) => {
 
     let img = doc.openImage(pathToImg);
-    doc.addPage({ size: [width, height] });
-    doc.image(img, 0, 0,
-        {
-            width: doc.page.width,
-            height: doc.page.height
-        })
+    doc.addPage({ size: [imgWidth, height] });
+    if (imgWidth > MAX_IMG_WIDTH) {
+        doc.image(img, (imgWidth - MAX_IMG_WIDTH) / 2, 0,
+            {
+                width: MAX_IMG_WIDTH,
+                height: doc.page.height
+            })
+    }
+    else {
+        doc.image(img, 0, 0,
+            {
+                width: imgWidth,
+                height: doc.page.height
+            })
+    }
 }
 
 export const addTextToSecondPage = (doc: any, text: string, width: number, height: number) => {
@@ -81,23 +91,7 @@ export const addTextToSecondPage = (doc: any, text: string, width: number, heigh
     doc.page.margins.bottom = oldBottomMargin; // ReProtect bottom margin
 }
 
-const text = `This PDF you are browsing is in a series of several scanned documents containing 
-the collation of all research material of Prof. Kul Bhushan Mohtra ji.Mohtra ji is currently the State 
-Incharge Library and Documentation Department, J&K BJP Headquarters, Nanaji 
-Deshmukh Library..This material was gathered while he was working on his multiple books on J&K History.
-All this rare material is now offered to the Community freely.<br>
 
-CV:<br>
-Kul Bhushan Mohtra was born on 9th Sep, 1957 in a village Amuwala in Kathua district.<br>
-Matric from BOSE, Jammu and Adeeb from AMU. Has been awarded Honorary Professor by School of Liberal Art & Languages, Shobhit University, Gangoh, Distt. Saharanpur, U.P.<br>
-Director General, Raja Ram Mohan Roy Library Foundation nominated him as his nominee in the Committee for purchasing of Books for UT Jammu & Kashmir. Incharge of Nanaji Deshmukh Library & Documentation Department at BJP state HQ in J&K.<br>
-Actively engaged in political, social, charitable and religious activities. Always striving to serve the poor and downtrodden of the society.<br>
-Main works-<br>
-A saga of Sacrifices: Praja Parishad Movement in J&K<br>
-100 Documents: A reference book J&K, Mission Accomplished<br>
-A Compendium of Icons of Jammu & Kashmir & our Inspiration (English)<br>
-Jammu Kashmir ki Sangarsh Gatha (Hindi)<br><br><br>
-Scanning and upload by eGangotri Foundation.<br>`.replace(/\n/g, '').replace(/<br>/g, '\n\n');
 
 
 const mergeVanityPdf = async (_introPdf: string, origPdf: string, finalDumpGround: string) => {
@@ -117,15 +111,13 @@ const mergeVanityPdf = async (_introPdf: string, origPdf: string, finalDumpGroun
 }
 
 (async () => {
-    const _root = "D:\\_Treasures59\\_data\\bjpjammu"
-    const _pdfRoot = `${_root}\\MohtraArchives`
-    const imgFile = `${_root}\\mohtra.jpg`
+    //Change values in constants.ts and then run yarn run vanity 
 
     const _pdfs = await getAllPdfsInFolders([_pdfRoot]);
     const intros: string[] = []
     for (let i = 0; i < _pdfs.length; i++) {
         console.log(`creating vanity for: ${_pdfs[i]}`, await PdfLibUtils.getPdfFirstPageDimensionsUsingPdfLib(_pdfs[i]))
-        intros.push(await createIntroPageWithImage(imgFile, _pdfs[i], text));
+        intros.push(await createIntroPageWithImage(imgFile, _pdfs[i], introText));
     }
     for (let i = 0; i < _pdfs.length; i++) {
         console.log(`creating vanity for: ${_pdfs[i]}`, await PdfLibUtils.getPdfFirstPageDimensionsUsingPdfLib(_pdfs[i]))
---
+++ src/vanityService/constants.ts
@@ -0,0 +1,38 @@
+ // const _root = "D:\\_Treasures59\\_data\\bjpjammu"
+ export const _root = "D:\\_Treasures59\\_data\\eg_books"
+ export const _pdfRoot = `${_root}`
+ export const imgFile = `${_root}\\Shail.jpg`
+export const MAX_IMG_WIDTH = 700;
+
+ const text = `This PDF you are browsing is in a series of several scanned documents containing 
+ the personal colleciton of Shailendra Ghulati ji from Jammu.<br>
+ CV:<br>
+ Shailendra Gulhati who is a Shaivite author of 6 books, used to learn finer aspects of Kashmir Shaiv Darsana from 
+ B N Panditji in the year 1995 in panditji’s Jammu residence. <br>
+ Ballajinath ji gave Shailendra a photocopy of his hand written notes on Siva sutras.<br>
+ He also reviewed Shailendra’s first book "The Yogi and the Snake".<br>
+ Shailendra is an online guide for Shaivism and may be reached on his Facebook page
+https://www.facebook.com/shailgulhatishivapage<br>
+Whilst his books are available on Amazon.<br>
+Scanning and upload by eGangotri Foundation.<br>`
+
+const text2 = `This PDF you are browsing is in a series of several scanned documents containing 
+the collation of all research material of Prof. Kul Bhushan Mohtra ji.Mohtra ji is currently the State 
+Incharge Library and Documentation Department, J&K BJP Headquarters, Nanaji 
+Deshmukh Library..This material was gathered while he was working on his multiple books on J&K History.
+All this rare material is now offered to the Community freely.<br>
+
+CV:<br>
+Kul Bhushan Mohtra was born on 9th Sep, 1957 in a village Amuwala in Kathua district.<br>
+Matric from BOSE, Jammu and Adeeb from AMU. Has been awarded Honorary Professor by School of Liberal Art & Languages, Shobhit University, Gangoh, Distt. Saharanpur, U.P.<br>
+Director General, Raja Ram Mohan Roy Library Foundation nominated him as his nominee in the Committee for purchasing of Books for UT Jammu & Kashmir. Incharge of Nanaji Deshmukh Library & Documentation Department at BJP state HQ in J&K.<br>
+Actively engaged in political, social, charitable and religious activities. Always striving to serve the poor and downtrodden of the society.<br>
+Main works-<br>
+A saga of Sacrifices: Praja Parishad Movement in J&K<br>
+100 Documents: A reference book J&K, Mission Accomplished<br>
+A Compendium of Icons of Jammu & Kashmir & our Inspiration (English)<br>
+Jammu Kashmir ki Sangarsh Gatha (Hindi)<br><br><br>
+Scanning and upload by eGangotri Foundation.<br>`
+
+export const introText = text.replace(/\n/g, '').replace(/<br>/g, '\n\n');
+
