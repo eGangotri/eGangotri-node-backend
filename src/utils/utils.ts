4ad7d19095bc3e92b3c0b7051940746db06554b6
egangotri <egangotritrust@gmail.com>
2023-07-18 01:52:13 +0800

    str

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,36 @@
-export const Utils = {};
+import fs from 'fs';
+import path from 'path';export const Utils = {};
 
 export const DD_MM_YYYY_FORMAT = 'DD-MMM-YYYY'
 export const DD_MM_YYYY_HH_MMFORMAT = 'DD-MMM-YYYY-HH-mm'
+
+function findLongestFileName(directory: string): string {
+  let longestFileName = '';
+
+  function processDirectory(dir: string) {
+    const files = fs.readdirSync(dir);
+
+    for (const file of files) {
+      const filePath = path.join(dir, file);
+      const stat = fs.statSync(filePath);
+
+      if (stat.isDirectory()) {
+        processDirectory(filePath);
+      } else {
+        const fileName = path.basename(filePath);
+        if (fileName.length > longestFileName.length) {
+          longestFileName = fileName;
+        }
+      }
+    }
+  }
+
+  processDirectory(directory);
+
+  return longestFileName;
+}
+
+const folderPath = "E:\\_catalogWork\\_reducedPdfs\\Treasures54 (1230)"
+const longestFileName = findLongestFileName(folderPath);
+console.log('Longest file name:', longestFileName);
+
