c8439558b794f2dbfb5fba442f5df54ab0d8ae54
eGangotri <egangotritrust@gmail.com>
2023-07-21 22:56:31 +0530

    aggregation

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -39,6 +39,48 @@ export const CSV_HEADER = [
     
   ];
   
+  export const CSV_HEADER_API2_SUBSET = [    {
+    id: "operatorName",
+    title: "Operator Name",
+  },
+  {
+    id: "center",
+    title: "Center",
+  },
+  {
+    id: "lib",
+    title: "Lib",
+  },
+  {
+    id: "totalPdfCount",
+    title: "TOTAL PDF COUNT",
+  },
+  {
+    id: "totalPageCount",
+    title: "TOTAL PAGE COUNT(Pages)",
+  },
+  {
+    id: "totalSize",
+    title: "TOTAL SIZE",
+  },
+  {
+    id: "totalSizeRaw",
+    title: "TOTAL SIZE (Raw)",
+  },
+  {
+    id: "workFromHome",
+    title: "Work From Home",
+  }]
+  
+  export const CSV_HEADER_API2_FOR_AGGREGATES = 
+  [
+    {
+      id: "numEntries",
+      title: "Number of Entries",
+    },
+    ...CSV_HEADER_API2_SUBSET
+  ]
+
   export const CSV_HEADER_API2 = 
   [
     
@@ -46,40 +88,9 @@ export const CSV_HEADER = [
       id: "dateOfReport",
       title: "Date of Report",
     },
-    {
-      id: "operatorName",
-      title: "Operator Name",
-    },
-    {
-      id: "center",
-      title: "Center",
-    },
-    {
-      id: "lib",
-      title: "Lib",
-    },
-    {
-      id: "totalPdfCount",
-      title: "TOTAL PDF COUNT",
-    },
-    {
-      id: "totalPageCount",
-      title: "TOTAL PAGE COUNT(Pages)",
-    },
-    {
-      id: "totalSize",
-      title: "TOTAL SIZE",
-    },
-    {
-      id: "totalSizeRaw",
-      title: "TOTAL SIZE (Raw)",
-    },
-    {
-      id: "workFromHome",
-      title: "Work From Home",
-    }
+    ...CSV_HEADER_API2_SUBSET
   ]
-  
+
   export const CSV_HEADER_THREE_FIELDS_ONLYAPI2 = 
   [
     {
