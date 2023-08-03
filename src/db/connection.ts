5dfa9cd1e00ff5040e17b4a2ac027b489e37a9b5
eGangotri <egangotritrust@gmail.com>
2023-08-04 00:54:15 +0530

    .

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -3,15 +3,17 @@ import * as _ from 'lodash';
 dotenv.config()
 const CONFS = process.env
 
+export let GLOBAL_DB_NAME = CONFS.MONGO_DB_NAME;
+
 export const mongoDbUrlWithDbName = (dbName: string) => {
     if (dbName === "forUpload") {
-        dbName = CONFS.MONGO_DB_NAME_FOR_UPLOAD || ""
+        GLOBAL_DB_NAME = CONFS.MONGO_DB_NAME_FOR_UPLOAD || ""
     }
     else {
-        dbName = CONFS.MONGO_DB_NAME || ""
+        GLOBAL_DB_NAME = CONFS.MONGO_DB_NAME || ""
     }
 
-    return `${CONFS.MONGO_DB_PROTOCOL}${CONFS.MONGO_ATLAS_USER}:${CONFS.MONGO_ATLAS_PWD}@${CONFS.MONGO_DB_PATH}/${dbName}${CONFS.MONGO_DB_SUFFIXES}`;
+    return `${CONFS.MONGO_DB_PROTOCOL}${CONFS.MONGO_ATLAS_USER}:${CONFS.MONGO_ATLAS_PWD}@${CONFS.MONGO_DB_PATH}/${GLOBAL_DB_NAME}${CONFS.MONGO_DB_SUFFIXES}`;
 }
 export const MONGO_OPTIONS = {
     useNewUrlParser: true,
