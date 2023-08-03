6f7bf5f87c1871ef2f8494636e66c599df539c0f
egangotri <egangotritrust@gmail.com>
2023-08-04 05:24:05 +0800

    .

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -7,7 +7,7 @@ export const Utils = {};
 export const DD_MM_YYYY_FORMAT = 'DD-MMM-YYYY'
 export const DD_MM_YYYY_HH_MMFORMAT = 'DD-MMM-YYYY-HH-mm'
 
-function findLongestFileName(directory: string): string {
+function findLongestFileName(directory: string, longestButLessThan: number = 300): string {
   let longestFileName = '';
 
   function processDirectory(dir: string) {
@@ -20,8 +20,9 @@ function findLongestFileName(directory: string): string {
       if (stat.isDirectory()) {
         processDirectory(filePath);
       } else {
-        const fileName = path.basename(filePath);
-        if (fileName.length > longestFileName.length) {
+        const fileName = path.basename(filePath); 
+        const fileWithPathLength = path.resolve(filePath);
+        if (fileName.length > longestFileName.length && (fileName.length < longestButLessThan)) {
           longestFileName = fileName;
         }
       }
@@ -59,10 +60,11 @@ function findHighestFileSize(directory: string): number {
   return highestSize;
 }
 
-/*
-const folderPath = "D:\\eG-tr1-30\\Treasures"
-const longestFileName = findLongestFileName(folderPath);
+const folderPath = "E:\\_catalogWork\\_reducedPdfs\\Treasures60 (1694)"
+const longestFileName = findLongestFileName(folderPath, 196);
 console.log('Longest file name:', longestFileName, longestFileName.length);
+
+/*
 
 const highestSize = findHighestFileSize(folderPath);
 console.log('Highest file size:', sizeInfo(highestSize));
