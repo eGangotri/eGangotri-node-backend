9121cb98acda64b4175b39bb96b750ac5150af2a
eGangotri <egangotritrust@gmail.com>
2023-08-25 23:11:31 +0530

    .

--- src/googleapi/GoogleDriveApiReadAndExport.ts
+++ src/googleapi/GoogleDriveApiReadAndExport.ts
@@ -1,29 +1,10 @@
-import { google } from 'googleapis';
 import { _credentials } from './_utils/credentials_gitignore';
-import * as fs from 'fs';
 import { listFolderContentsAndGenerateCSVAndExcel } from './service/GoogleApiService';
-import * as FileUtils from "../imgToPdf/utils/FileUtils";
+import { getGoogleDriveInstance } from './service/CreateGoogleDrive';
 
-// Set up OAuth2 credentials
-const credentials = {
-  ..._credentials,
-  refresh_token: '1//0gNBMXupFzUULCgYIARAAGBASNwF-L9IrnF4rkWHpvRSV1TqBV7ujMRHZP-biHpMFJQvpWW-4e5dlNGlNyGfnCW0ywWv3XLkHtmc',
-};
-
-// Create an OAuth2 client
-const oauth2Client = new google.auth.OAuth2(
-  credentials.client_id,
-  credentials.client_secret,
-  credentials.redirect_uris[0]
-);
-
-// Set the credentials for the OAuth2 client
-oauth2Client.setCredentials({
-  refresh_token: credentials.refresh_token,
-});
 
 // Create a new Google Drive instance
-const drive = google.drive({ version: 'v3', auth: oauth2Client });
+const drive = getGoogleDriveInstance();
 
 
 async function procOrigGoogleDrive(driveLinkOrFolderID: string, folderName: string) {
@@ -39,8 +20,8 @@ const EXPORT_ROOT_FOLDER = `C:\\Users\\chetan\\Documents\\_personal\\`;
 
 //all entries must have await in front
 (async () => {
-  await procOrigGoogleDrive("https://drive.google.com/drive/folders/1EJ2drXCdUhEUqlkr5AQvhnZ9vC5eODnU?usp=drive_link",
-   'procFolderSvshastri');
+  await procOrigGoogleDrive("https://drive.google.com/drive/folders/1T4orchOqirs-vPc-yydnsfsmHhvszl92?usp=drive_link",
+    'procFolderSvshastri2');
   ////await procReducedPdfGoogleDrive("1Nox5h2CYgIrGcd73JswHk0_q05y0W-b7", 'Treasures60');
 })();
 
