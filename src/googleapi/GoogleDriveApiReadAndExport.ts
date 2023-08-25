14581dbd1059924d9e471f8f03bf89bd8162adce
eGangotri <egangotritrust@gmail.com>
2023-08-26 00:18:22 +0530

    Google Pdf Download

--- src/googleapi/GoogleDriveApiReadAndExport.ts
+++ src/googleapi/GoogleDriveApiReadAndExport.ts
@@ -3,10 +3,8 @@ import { listFolderContentsAndGenerateCSVAndExcel, listFolderContentsAsArrayOfDa
 import { getGoogleDriveInstance } from './service/CreateGoogleDrive';
 import { downloadPdfFromGoogleDrive } from '../pdf/downloadPdf';
 
-
 // Create a new Google Drive instance
 const drive = getGoogleDriveInstance();
-
 
 async function procOrigGoogleDrive(driveLinkOrFolderID: string, folderName: string) {
   await listFolderContentsAndGenerateCSVAndExcel(driveLinkOrFolderID, drive, `${EXPORT_ROOT_FOLDER}_googleDriveExcels`, folderName);
@@ -16,23 +14,5 @@ async function procReducedPdfGoogleDrive(driveLinkOrFolderID: string, folderName
   await listFolderContentsAndGenerateCSVAndExcel(driveLinkOrFolderID, drive, `${EXPORT_ROOT_FOLDER}_catReducedDrivePdfExcels`, folderName);
 }
 
-async function getAllPdfs(driveLinkOrFolderID: string, folderName: string) {
-  const googleDriveData = await listFolderContentsAsArrayOfData(driveLinkOrFolderID, drive, `${EXPORT_ROOT_FOLDER}_googleDriveExcels`, folderName);
-  if(googleDriveData.length > 10){
-    console.log("restriction to 10 items only for now. exiting")
-    process.exit(0);
-  }
-  googleDriveData.map((x) => downloadPdfFromGoogleDrive(x.googleDriveLink))
-}
-
-//const EXPORT_ROOT_FOLDER = `C:\\_catalogWork\\_collation\\`;
-const EXPORT_ROOT_FOLDER = `C:\\Users\\chetan\\Documents\\_personal\\`;
-
-//all entries must have await in front
-(async () => {
-  await getAllPdfs("1eJnYKRgZIyPO2s-BgsJ4ozhCEuH3i_lQ",
-    'procFolderSvshastri3');
-  ////await procReducedPdfGoogleDrive("1Nox5h2CYgIrGcd73JswHk0_q05y0W-b7", 'Treasures60');
-})();
-
+const EXPORT_ROOT_FOLDER = `C:\\_catalogWork\\_collation\\`;
 //yarn run catalog
