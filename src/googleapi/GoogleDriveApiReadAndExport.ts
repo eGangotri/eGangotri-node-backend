ed4f9e8fc7664260b5d3f424c28c881cdb9637f4
eGangotri <egangotritrust@gmail.com>
2023-09-07 22:01:32 +0530

    making lean

--- src/googleapi/GoogleDriveApiReadAndExport.ts
+++ src/googleapi/GoogleDriveApiReadAndExport.ts
@@ -1,7 +1,6 @@
 import { _credentials } from './_utils/credentials_googleapi';
 import { listFolderContentsAndGenerateCSVAndExcel, listFolderContentsAsArrayOfData } from './service/GoogleApiService';
 import { getGoogleDriveInstance } from './service/CreateGoogleDrive';
-import { downloadPdfFromGoogleDrive } from '../pdf/downloadPdf';
 
 // Create a new Google Drive instance
 const drive = getGoogleDriveInstance();
