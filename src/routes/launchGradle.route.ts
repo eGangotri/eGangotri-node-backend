a360e0d85f1989fa3c4c6a2c1b1a952c53030d88
eGangotri <egangotritrust@gmail.com>
2023-09-18 00:01:58 +0530

    moveToFreeze Gradle Task

--- src/routes/launchGradle.route.ts
+++ src/routes/launchGradle.route.ts
@@ -1,13 +1,13 @@
 import * as express from 'express';
 import { launchUploader, moveToFreeze } from '../services/gradleLauncherService';
 
-export const launchGradleRoute =  express.Router();
+export const launchGradleRoute = express.Router();
 
-launchGradleRoute.get('/', async (req:any, resp:any) => {
+launchGradleRoute.get('/', async (req: any, resp: any) => {
     try {
         launchUploader(req.query.profiles)
         resp.status(200).send("Success");
-        
+
     }
     catch (err: any) {
         console.log('Error', err);
@@ -15,13 +15,12 @@ launchGradleRoute.get('/', async (req:any, resp:any) => {
     }
 })
 
-launchGradleRoute.get('/moveToFreeze', async (req:any, resp:any) => {
+launchGradleRoute.get('/moveToFreeze', async (req: any, resp: any) => {
     try {
         const _profiles = req.query.profiles
         console.log(`moveToFreeze ${_profiles}`)
-        moveToFreeze(req.query.profiles)
-        resp.status(200).send("Success");
-        
+        const res = await moveToFreeze(req.query.profiles)
+        resp.status(200).send(res);
     }
     catch (err: any) {
         console.log('Error', err);
--- src/services/gradleLauncherService.ts
+++ src/services/gradleLauncherService.ts
@@ -25,20 +25,28 @@ export function launchUploader(args: any) {
 }
 
 //localhost/launchGradle/moveToFreeze?profiles="TEST,TMP"
-export function moveToFreeze(args: any) {
+export function moveToFreeze(args: any):Promise<string> {
     console.log(`args ${args}`);
 
-    exec(`gradle fileMover --args=${args.split(",").join(" ")}`, {
-        cwd: `${WORKING_DIR}`
-    }, (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    })
+    //gradle fileMover --args=JNGM_BEN JNGM_TAMIL JNGM_TEL JNGM BVT
+    const _cmd = `gradle fileMover --args=${args.split(",").join(" ")}`
+    return new Promise((resolve, reject) => {
+        exec(_cmd, {
+            cwd: `${WORKING_DIR}`
+        }, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                reject(error);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                reject(new Error(stderr));
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            resolve(stdout);
+        })
+    });
+
 }
