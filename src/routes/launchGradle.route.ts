87d7f07b32b3cebbb04c35b64abee15c5c15a1ed
eGangotri <egangotritrust@gmail.com>
2023-09-17 23:36:15 +0530

    moveToFreeze Gradle Task

--- src/routes/launchGradle.route.ts
+++ src/routes/launchGradle.route.ts
@@ -1,12 +1,25 @@
 import * as express from 'express';
-import { launchUploader } from '../services/gradleLauncherService';
+import { launchUploader, moveToFreeze } from '../services/gradleLauncherService';
 
 export const launchGradleRoute =  express.Router();
-
 
 launchGradleRoute.get('/', async (req:any, resp:any) => {
     try {
         launchUploader(req.query.profiles)
+        resp.status(200).send("Success");
+        
+    }
+    catch (err: any) {
+        console.log('Error', err);
+        resp.status(400).send(err);
+    }
+})
+
+launchGradleRoute.get('/moveToFreeze', async (req:any, resp:any) => {
+    try {
+        const _profiles = req.query.profiles
+        console.log(`moveToFreeze ${_profiles}`)
+        moveToFreeze(req.query.profiles)
         resp.status(200).send("Success");
         
     }
--- src/services/gradleLauncherService.ts
+++ src/services/gradleLauncherService.ts
@@ -2,14 +2,33 @@ import { exec, spawn } from 'child_process';
 import { WORKING_DIR } from '../common';
 
 
-export function launchUploader(args:any) {
+export function launchUploader(args: any) {
     console.log(`args ${args}`);
-    if(!args){
+    if (!args) {
         args = "C:\\Users\\Chetan Pandey\\Downloads\\forPrint"
     }
 
- //    exec(`gradlew loginToArchive --args==` + args, {
-      exec(`gradlew bookTitles --args==${args}`, {
+    //    exec(`gradlew loginToArchive --args==` + args, {
+    exec(`gradle bookTitles --args=${args}`, {
+        cwd: `${WORKING_DIR}`
+    }, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    })
+}
+
+//localhost/launchGradle/moveToFreeze?profiles="TEST,TMP"
+export function moveToFreeze(args: any) {
+    console.log(`args ${args}`);
+
+    exec(`gradle fileMover --args=${args.split(",").join(" ")}`, {
         cwd: `${WORKING_DIR}`
     }, (error, stdout, stderr) => {
         if (error) {
