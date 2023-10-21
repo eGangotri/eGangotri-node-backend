5ee260998844096799070b30413af238f8e3aff1
writetochetanpandey <writetochetanpandey@gmail.com>
2023-10-21 16:46:08 +0530

    -

---
+++ src/cliBased/constants.ts
@@ -0,0 +1,10 @@
+import path from 'path'
+
+export const SRC_ROOT = "SRC_ROOT"
+export const DEST_ROOT = "DEST_ROOT"
+export const DEST_OTRO_ROOT = "DEST_OTRO_ROOT"
+
+export const USER_HOME = process.env['HOME'];
+export const EGANGOTRI_BASE_DIR = `${USER_HOME}${path.sep}eGangotri`;
+export const PROPERTIES = ".properties"
+export const LOCAL_FOLDERS_PROPERTIES_FILE = `${EGANGOTRI_BASE_DIR}${path.sep}localFolders${PROPERTIES}`;
