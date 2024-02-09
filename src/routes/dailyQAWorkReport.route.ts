3116c5ed955a514c24c145c3ebefeb98fa80943e
writetochetanpandey <writetochetanpandey@gmail.com>
2024-02-09 12:03:19 +0530

    QA

---
+++ src/routes/dailyQAWorkReport.route.ts
@@ -0,0 +1,236 @@
+const express = require("express");
+import { generateQAWorkReportCSV, generateQAWorkReportCSVApi2 } from "../services/CsvUtil";
+import { deleteRowsByIds } from "../services/dailyWorkReportService";
+import { Request, Response } from "express";
+import { validateSuperAdminUserFromRequest, validateUserFromRequest } from "../services/userService"
+import { getDateTwoHoursBeforeNow, replaceQuotesAndSplit } from "../services/Util";
+import _ from "lodash";
+import { generateQACSVAsFile, generateQACSVAsFileForOperatorAndEntryCountOnly, generateQACSVAsFileOfAggregates, getListOfDailyQAWorkReport } from "services/dailyQAWorkReportService";
+import { DailyQAWorkReport } from "models/dailyQAReport";
+
+export const dailyQAWorkReportRoute = express.Router();
+
+/**
+ * INSOMNIA POST Request Sample
+POST http://localhost/dailyWorkReport/add 
+JSON Body 
+{
+  "title": "eGangotri Daily QA Work Report",
+  "operatorName": "admin",
+  "catalogProfile": "Treasures-8",
+  "entryFrom": 1,
+  "entryTo": 11,
+  "skipped": 2,
+  "timeOfRequest": "2023-07-18T09:30:20.401Z",
+  "entryCount": 8,
+  "link": "https://docs.google.com/spreadsheets/d/1masb0zc_bvOYU1r70cjf6sPvExbF5bF6s_VDPeBH4KY/edit#gid=0",
+  "notes": "",
+  "password": ""
+}
+*/
+
+dailyQAWorkReportRoute.post("/add", async (req: Request, resp: Response) => {
+  try {
+    const operatorName = req.body.operatorName
+    if (operatorName) {
+      resp.status(400).send({ error: "Non-functional" });
+    }
+
+    else {
+      if (await validateUserFromRequest(req)) {
+        const dailyQAWorkReport = new DailyQAWorkReport(req.body);
+
+        //Check if any request was sent in Last 2 hours
+        const _query: typeof req.query = {};
+        _query['isLastTwoHours'] = 'true';
+        _query['operatorName'] = operatorName;
+
+        const preExisting = await getListOfDailyQAWorkReport(_query);
+        if (preExisting && preExisting.length >= 1) {
+          console.log(` preExisting[0]._id . ${preExisting.length}`);
+          const filter = {
+            _id: { $in: preExisting.map(x => x._id) },
+            //just to be extra safe
+            createdAt: {
+              $gte: new Date(getDateTwoHoursBeforeNow()),
+              $lte: new Date(new Date()),
+            },
+          };
+          await DailyQAWorkReport.deleteMany(filter);
+          await dailyQAWorkReport.save();
+          resp.status(200).send({
+            "warning": `Since Last Submission Request < 2 Hours. exisiting Data is merely overwritten not inserted. for ${operatorName}`
+          });
+
+        }
+        else {
+          console.log(`dailyQAWorkReportRoute /add ${JSON.stringify(dailyQAWorkReport)}`);
+          await dailyQAWorkReport.save();
+          resp.status(200).send({
+            "success": `Added Daily Report Stats with Id ${dailyQAWorkReport._id} for ${operatorName}`
+          });
+        }
+      }
+      else {
+        resp.status(200).send({ error: `Couldn't validate User ${operatorName}` });
+      }
+    }
+  }
+  catch (err: any) {
+    console.log("Error", err);
+    resp.status(400).send({ error: err });
+  }
+});
+
+dailyQAWorkReportRoute.get("/list", async (req: Request, resp: Response) => {
+  try {
+    const items = await getListOfDailyQAWorkReport(req?.query);
+    console.log(
+      `after getListOfDailyWorkReport retrieved item count: ${items.length}`
+    );
+    resp.status(200).send({
+      response: items,
+    });
+  } catch (err: any) {
+    console.log("Error", err);
+    resp.status(400).send(err);
+  }
+});
+
+
+dailyQAWorkReportRoute.get("/listIds", async (req: Request, resp: Response) => {
+  try {
+    const items = await getListOfDailyQAWorkReport(req?.query);
+    console.log(
+      `after getListOfDailyWorkReport retrieved item count: ${items.length}`
+    );
+    const itemIds = items?.map((item) => item._id);
+    resp.status(200).send({
+      response: itemIds,
+      responseAsCsv: itemIds?.join(","),
+    });
+  } catch (err: any) {
+    console.log("Error", err);
+    resp.status(400).send(err);
+  }
+});
+/**
+ * 
+ * {
+  "_ids": "64bf2382c0a3e0e17c64b472,64bbc572c0a3e0e17c64b124,64bbc542c0a3e0e17c64b123",
+  "superadmin_user": "",
+  "superadmin_password": ""
+}
+ */
+//non-functional
+dailyQAWorkReportRoute.delete("/delete", async (req: Request, resp: Response) => {
+  try {
+    const _ids = req.body?._ids || "";
+    const idsAsCSV = replaceQuotesAndSplit(_ids);
+    const _validate = await validateSuperAdminUserFromRequest(req);
+    if (_validate[0]) {
+      if (_.isEmpty(_ids)) {
+        console.log(`cannot proceed _id not provided`);
+        resp.status(300).send({
+          response: `Must have param '_ids' containing one or more id as coma-separated value`,
+        });
+      }
+      else {
+        if (idsAsCSV.length > 10) {
+          resp.status(300).send({
+            response: `Expected deletion of ${idsAsCSV.length} items but more than 10 is not allowed`,
+          });
+        }
+        const items = await getListOfDailyQAWorkReport(req.body);
+        console.log(
+          `after getListOfDailyWorkReport retrieved item count: ${items.length}`
+        );
+
+        if (_.isEmpty(items)) {
+          resp.status(200).send({
+            response: "No _ids matching found. No deletion",
+          });
+        }
+        else {
+          const _idsfiltered = items.filter((item) => idsAsCSV.includes(item._id.toString())).map((x) => x._id.toString())
+          console.log(`_ids2 ${_idsfiltered.length} idsAsCSV.length ${idsAsCSV.length}`)
+          const status = await deleteRowsByIds(_idsfiltered);
+          resp.status(200).send({
+            response: `Deleted ${status.deletedCount} items from ${idsAsCSV.length} _ids provided`,
+            message: `${JSON.stringify(status)}`
+          });
+        }
+      }
+    }
+    else {
+      resp.status(200).send({ error: _validate[1] });
+    }
+  } catch (err: any) {
+    console.log("Error", err);
+    resp.status(400).send(err);
+  }
+});
+
+//localhost/dailyWorkReport/csv?startDate="1-Jan-2023"&endDate="31-Jun-2023"
+dailyQAWorkReportRoute.get("/csv", async (req: Request, resp: Response) => {
+  try {
+    const items = await getListOfDailyQAWorkReport(req?.query);
+    console.log(
+      `after getListOfDailyQAWorkReport retrieved item count: ${items[0]}`
+    );
+    const _csv = generateQAWorkReportCSV(items)
+    resp.status(200).send(_csv);
+  } catch (err: any) {
+    console.log("Error", err);
+    resp.status(400).send(err);
+  }
+});
+
+//localhost/dailyWorkReport/csvAsJsonArray?startDate="1-Jan-2023"&endDate="31-Jun-2023"
+dailyQAWorkReportRoute.get("/csvAsJsonArray", async (req: Request, resp: Response) => {
+  try {
+    const items = await getListOfDailyQAWorkReport(req?.query);
+    console.log(
+      `after getListOfDailyWorkReport retrieved item count: ${items[0]}`
+    );
+    const _csv = generateQAWorkReportCSVApi2(items)
+    resp.status(200).send(_csv);
+  } catch (err: any) {
+    console.log("Error", err);
+    resp.status(400).send(err);
+  }
+});
+
+dailyQAWorkReportRoute.get("/csvAsFile", async (req: Request, resp: Response) => {
+  try {
+    const items = await getListOfDailyQAWorkReport(req?.query);
+    console.log(
+      `after getListOfDailyWorkReport retrieved item count: ${items.length}`
+    );
+
+    if (req?.query?.aggregations === "true") {
+      generateQACSVAsFileOfAggregates(resp, items)
+    }
+    else {
+      generateQACSVAsFile(resp, items)
+    }
+  } catch (err: any) {
+    console.log("Error", err);
+    resp.status(400).send(err);
+  }
+});
+
+dailyQAWorkReportRoute.get("/csvAsFileOnlyOperatorAndPdfCount", async (req: Request, resp: Response) => {
+  try {
+    const items = await getListOfDailyQAWorkReport(req?.query);
+    console.log(
+      `after getListOfDailyWorkReport retrieved item count: ${items.length}`
+    );
+
+    generateQACSVAsFileForOperatorAndEntryCountOnly(resp, items)
+  } catch (err: any) {
+    console.log("Error", err);
+    resp.status(400).send(err);
+  }
+});
+
---
+++ src/services/dailyQAWorkReportService.ts
@@ -0,0 +1,211 @@
+import mongoose from "mongoose";
+import moment from 'moment';
+import { DailyQAWorkReportListOptionsType } from "./types";
+import { getLimit } from "../routes/utils";
+
+import { Response } from "express";
+import { createObjectCsvWriter } from "csv-writer";
+import { createReadStream } from "fs";
+import * as fs from "fs";
+import { CSV_CAT_HEADER_API2, CSV_CAT_HEADER_FOR_AGGREGATES_API2, CSV_CAT_HEADER_FOR_OPERATOR_NAME_AND_ENTRY_COUNT_API2 } from "./constants";
+import * as _ from 'lodash';
+import { generateCsvDirAndName, getDateTwoHoursBeforeNow, replaceQuotes, replaceQuotesAndSplit } from "./Util";
+import { DailyQAWorkReport } from "models/dailyQAReport";
+
+
+
+export function setOptionsForDailyQAWorkReportListing(queryOptions: DailyQAWorkReportListOptionsType) {
+  // Empty `filter` means "match all documents"
+  // mongoOptionsFilter: {"dateOfReport":{"$gte":"2023-06-22T18:30:00.000Z","$lte":"2023-06-24T18:29:59.000Z"}}
+  let mongoOptionsFilter = {};
+  if (queryOptions?.startDate && queryOptions?.endDate && moment(queryOptions?.startDate).isValid() && moment(queryOptions?.endDate).isValid()) {
+    const startDateWithTimeComponent = new Date(replaceQuotes(queryOptions?.startDate) + " 00:00:00");
+    const endDateWithTimeComponent = new Date(replaceQuotes(queryOptions?.endDate) + " 23:59:59");
+    console.log(`endDateWithTimeComponent ${endDateWithTimeComponent}  ${new Date()}`)
+    mongoOptionsFilter = {
+      createdAt: {
+        $gte: startDateWithTimeComponent,
+        $lte: endDateWithTimeComponent,
+      },
+    };
+  }
+
+  if (queryOptions?.operatorName) {
+    const operatorName: string[] = replaceQuotesAndSplit(queryOptions.operatorName)
+    //This wil make the operatorName case-independent
+    var regexArray = operatorName.map(pattern => new RegExp(`^${pattern}$`, 'i'));
+    mongoOptionsFilter = { ...mongoOptionsFilter, operatorName: { $in: regexArray } };
+  }
+
+
+  if (queryOptions?.catalogProfile) {
+    const centers: string[] = replaceQuotesAndSplit(queryOptions.catalogProfile)
+    //This wil make the username case-independent
+    var regexArray = centers.map(pattern => new RegExp(`^${pattern}$`, 'i'));
+    mongoOptionsFilter = { ...mongoOptionsFilter, centcatalogProfileer: { $in: regexArray } };
+  }
+
+  if (queryOptions?._ids) {
+    const _id: string[] = replaceQuotesAndSplit(queryOptions._ids);
+    mongoOptionsFilter = { ...mongoOptionsFilter, _id: { $in: _id } };
+  }
+  if (queryOptions?.isLastTwoHours) {
+    const isLastTwoHours = replaceQuotes(queryOptions.isLastTwoHours);
+    if (isLastTwoHours === 'true') {
+      mongoOptionsFilter = {
+        ...mongoOptionsFilter,
+        createdAt: {
+          $gte: new Date(getDateTwoHoursBeforeNow()),
+          $lte: new Date(new Date()),
+        },
+      };
+    }
+  }
+
+  const limit: number = getLimit(queryOptions?.limit);
+  console.log(`mongoOptionsFilter: ${JSON.stringify(mongoOptionsFilter)}`)
+  console.log(`User dispatched queryOptions ${JSON.stringify(queryOptions)}`)
+  return { limit, mongoOptionsFilter };
+}
+
+export async function getListOfDailyQAWorkReport(queryOptions: DailyQAWorkReportListOptionsType) {
+  const { limit, mongoOptionsFilter } = setOptionsForDailyQAWorkReportListing(queryOptions)
+
+  const items = await DailyQAWorkReport.find(mongoOptionsFilter)
+    .sort({ createdAt: -1 })
+    .limit(limit);
+  return items;
+}
+
+
+export const generateQACSVAsFile = async (res: Response, data: mongoose.Document[]) => {
+  const csvFileName = generateCsvDirAndName("QA");
+  try {
+    // Define the CSV file headers
+    const csvWriter = createObjectCsvWriter({
+      path: csvFileName,
+      header: CSV_CAT_HEADER_API2
+    });
+
+    const entryCountSum: number = _.sum(data.map(x => parseInt(x.get("entryCount").toString())))
+
+    console.log(`_toObj ${entryCountSum}`);
+    await csvWriter.writeRecords(data);
+    await csvWriter.writeRecords([{
+      timeOfRequest: "Totals",
+      operatorName: data.length,
+      catalogProfile: "",
+      entryFrom: "",
+      entryTo: "",
+      skipped: "",
+      entryCount: entryCountSum,
+    }]);
+
+    // Set the response headers to indicate that the response is a CSV file
+    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader('Content-Disposition', `attachment; filename=${csvFileName}`);
+
+    // Stream the CSV file as the response
+    const fileStream = createReadStream(csvFileName);
+    fileStream.pipe(res);
+  } catch (error) {
+    console.error('Error writing CSV:', error);
+    res.status(500).send('Internal Server Error');
+  }
+}
+
+
+export const generateQACSVAsFileOfAggregates = async (res: Response, data: mongoose.Document[]) => {
+  const csvFileName = generateCsvDirAndName("QAAggregates");
+  try {
+    // Define the CSV file headers
+    const csvWriter = createObjectCsvWriter({
+      path: csvFileName,
+      header: CSV_CAT_HEADER_FOR_AGGREGATES_API2
+    });
+
+    const grouped_data = _.groupBy(data, (doc) => {
+      return _.capitalize(doc.get("operatorName").toString().toLowerCase())
+    });
+
+    console.log(`grouped_data ${JSON.stringify(grouped_data)}`);
+
+    const refinedData: Array<any> = []
+    for (let [key, value] of Object.entries(grouped_data)) {
+      const numOfEntries = value.length;
+      const entryCountSum = _.sum(value.map(x => parseInt(x.get("entryCount")) || 0));
+      const average = Math.round(entryCountSum / numOfEntries)
+      const _row = {
+        numEntries: numOfEntries,
+        operatorName: key,
+        entryCount: entryCountSum,
+        average
+      }
+      refinedData.push(_row)
+    }
+    const entryCountSum: number = _.sum(refinedData.map(x => parseInt(x["entryCount"]?.toString())))
+    const numEntriesSum: number = _.sum(refinedData.map(x => x["numEntries"]))
+    const averageSum: number = _.sum(refinedData.map(x => x["average"]))
+
+    console.log(`_toObj ${entryCountSum} numEntriesSum ${numEntriesSum}`);
+    await csvWriter.writeRecords(refinedData);
+    await csvWriter.writeRecords([{
+      numEntries: numEntriesSum,
+      operatorName: refinedData.length,
+      entryCount: entryCountSum,
+      average: averageSum,
+    }]);
+
+    // Set the response headers to indicate that the response is a CSV file
+    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader('Content-Disposition', `attachment; filename=${csvFileName}`);
+
+    // Stream the CSV file as the response
+    const fileStream = createReadStream(csvFileName);
+    fileStream.pipe(res);
+  } catch (error) {
+    console.error('Error writing CSV:', error);
+    res.status(500).send('Internal Server Error');
+  }
+}
+export const generateQACSVAsFileForOperatorAndEntryCountOnly = async (res: Response, data: mongoose.Document[]) => {
+  const csvFileName = generateCsvDirAndName("QA");
+  try {
+    // Define the CSV file headers
+    const csvWriter = createObjectCsvWriter({
+      path: csvFileName,
+      header: CSV_CAT_HEADER_FOR_OPERATOR_NAME_AND_ENTRY_COUNT_API2
+    });
+
+    const entryCountSum: number = _.sum(data.map(x => parseInt(x.get("entryCount").toString())))
+
+    console.log(`_toObj ${entryCountSum}`);
+    await csvWriter.writeRecords(data);
+    await csvWriter.writeRecords([{
+      operatorName: data.length,
+      entryCount: entryCountSum,
+    }]);
+
+    // Set the response headers to indicate that the response is a CSV file
+    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader('Content-Disposition', `attachment; filename=${csvFileName}`);
+
+    // Stream the CSV file as the response
+    const fileStream = createReadStream(csvFileName);
+    fileStream.pipe(res);
+  } catch (error) {
+    console.error('Error writing CSV:', error);
+    res.status(500).send('Internal Server Error');
+  }
+}
+
+export const deleteRowsByIds = async (_itemIds: string[]) => {
+  // Define your criteria for deletion
+  const deleteCriteria = {
+    _id: { $in: _itemIds }
+  };
+  // Delete the rows based on the criteria
+  const res = await DailyQAWorkReport.deleteMany(deleteCriteria);
+  console.log(`delete res ${JSON.stringify(res)}`)
+  return res;
+}
