7d7ab8d3e1842ba5398e24d3a43835c412621798
writetochetanpandey <writetochetanpandey@gmail.com>
2024-03-27 00:43:00 +0530

    movefiles and flatten improved.

--- src/cliBased/fileMover/index.ts
+++ src/cliBased/fileMover/index.ts
@@ -1,4 +1,6 @@
+import { isFileInUse } from "../../archiveDotOrg/fileUtils";
 import { getAllPDFFiles } from "../../imgToPdf/utils/FileUtils";
+import { FileStats } from "imgToPdf/utils/types";
 
 const path = require('path');
 const fs = require('fs');
@@ -7,14 +9,46 @@ export async function moveFilesAndFlatten(sourceDir: string, targetDir: string) 
     //implement alogrithm
     //(1) check if any file is open or any file is already present in source Dir
     // if yes then send msg otherwise continut
-
+    if (sourceDir === targetDir) {
+        return {
+            msg: `sourceDir and targetDir are same, cancelling move operation`,
+            success: false
+        };
+    }
     console.log(`sourceDir ${sourceDir} targetDir ${targetDir}`);
     let counter = 0;
     let dirs = [sourceDir];
     const filesMoved = [];
     const fileCollision = [];
-    const _count = (await getAllPDFFiles(sourceDir))?.length;
+    const allSrcPdfs: FileStats[] = await getAllPDFFiles(sourceDir);
 
+    //check if any is in use
+    const filesInUse = allSrcPdfs.filter(file => isFileInUse(file.absPath));
+    if (filesInUse.length > 0) {
+        console.error(`Following files are in use, cancelling move operation: ${filesInUse.map(file => file.fileName)}`);
+        return {
+            success: false,
+            msg: `Following files are in use, cancelling move operation`,
+            filesInUse: filesInUse.map(file => file.absPath)
+        };
+    }
+
+    //check for collisions
+    const allDestPdfs: FileStats[] = await getAllPDFFiles(targetDir);
+    const allSrcFileNames = allSrcPdfs.map((x) => x.fileName);
+    const allDestFileNames = allDestPdfs.map((x) => x.fileName);
+    console.log(`allSrcFileNames ${allSrcFileNames}
+     allDestFileNames ${allDestFileNames}`)
+    const matchingFiles = allSrcFileNames.filter(file => allDestFileNames.includes(file));
+    if (matchingFiles.length > 0) {
+        console.error(`Following files are already present in target dir, cancelling move operation: ${matchingFiles}`);
+        return {
+            success: false,
+            msg: `Following files are already present in target dir, cancelling move operation`,
+            matchingFiles
+        };
+    }
+    const _count = allSrcPdfs?.length;
     while (dirs.length > 0) {
         const currentDir = dirs.pop();
         const files = fs.readdirSync(currentDir, { withFileTypes: true });
@@ -45,7 +79,7 @@ export async function moveFilesAndFlatten(sourceDir: string, targetDir: string) 
     const msg = `All files moved from Source dir ${sourceDir}(${_count})  to target dir ${targetDir}`
     console.log(msg);
     return {
-        msg ,
+        msg,
         filesMovedLength: filesMoved?.length,
         filesMoved
     };
