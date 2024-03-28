09c9a89a615f2fb90c3ee927662fbb09ccc75864
writetochetanpandey <writetochetanpandey@gmail.com>
2024-03-29 04:29:19 +0530

    movefiles and flatten improved.

--- src/services/yarnService.ts
+++ src/services/yarnService.ts
@@ -2,6 +2,8 @@ import { isValidPath } from "../utils/utils";
 import { moveFilesAndFlatten } from "../cliBased/fileMover";
 import { getFolderInDestRootForProfile, getFolderInSrcRootForProfile } from "../cliBased/utils";
 import * as fs from 'fs';
+import { getAllPDFFilesWithMedata } from "../imgToPdf/utils/FileUtils";
+import { FileStats } from "../imgToPdf/utils/types";
 
 export const moveProfilesToFreeze = async (profileAsCSV: string, flatten: boolean = true) => {
     const _response = []
@@ -50,4 +52,50 @@ export const moveFileSrcToDest = async (srcPath: string, destFolderOrProfile: st
             err: err
         };
     }
+}
+
+const getFoldersFromInput = (argFirst: string) => {
+    const profileOrPaths = argFirst.includes(",") ? argFirst.split(",").map((link: string) => link.trim()) : [argFirst.trim()];
+    console.log(`profileOrPaths ${profileOrPaths}`);
+
+    const pdfDumpFolders = isValidPath(profileOrPaths[0]) ? profileOrPaths :
+        profileOrPaths.map((_profileOrPath: string) => {
+            return getFolderInDestRootForProfile(_profileOrPath)
+        });
+
+    console.log(`pdfDumpFolders ${pdfDumpFolders}`)
+
+    return pdfDumpFolders
+}
+
+export const publishBookTitles = async (argFirst: string, pdfsOnly = true) => {
+    const pdfDumpFolders = getFoldersFromInput(argFirst);
+
+    const _response = []
+    for (let folder of pdfDumpFolders) {
+        if (isValidPath(folder)) {
+            const metadata = await getAllPDFFilesWithMedata(folder);
+            generateTextFile(metadata, pdfsOnly);
+            generateExcelOfPdfMetadata(metadata, pdfsOnly)
+            _response.push({success: true, msg: `Published book titles for ${folder}`});
+        }
+        else {
+            _response.push({
+                success: false,
+                msg: `Invalid folder ${folder}`
+            });
+        }
+    }
+    return {
+        response: _response
+    };
+}
+
+
+function generateTextFile(metadata: Array<FileStats>, pdfsOnly: boolean) {
+    console.log(`generateTextFile-InConstruction ${metadata.length} pdfsOnly ${pdfsOnly}`)
+}
+
+function generateExcelOfPdfMetadata(metadata: Array<FileStats>, pdfsOnly: boolean) {
+    console.log(`generateExcelOfPdfMetadata-InConstruction ${metadata.length} pdfsOnly ${pdfsOnly}`)
 }
