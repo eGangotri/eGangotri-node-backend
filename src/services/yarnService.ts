96846208b306417aa87c460ea99e000b10fd9396
writetochetanpandey <writetochetanpandey@gmail.com>
2024-04-01 03:07:50 +0530

    -

--- src/services/yarnService.ts
+++ src/services/yarnService.ts
@@ -4,6 +4,12 @@ import { getFolderInDestRootForProfile, getFolderInSrcRootForProfile } from "../
 import * as fs from 'fs';
 import { getAllPDFFilesWithMedata } from "../imgToPdf/utils/FileUtils";
 import { FileStats } from "../imgToPdf/utils/types";
+import { sizeInfo } from "../mirror/FrontEndBackendCommonCode";
+import * as path from 'path';
+import moment from "moment";
+import { DD_MM_YYYY_HH_MMFORMAT } from "../utils/constants";
+
+const _root = "C:\\_catalogWork\\_collation\\local";
 
 export const moveProfilesToFreeze = async (profileAsCSV: string, flatten: boolean = true) => {
     const _response = []
@@ -75,9 +81,9 @@ export const publishBookTitles = async (argFirst: string, pdfsOnly = true) => {
     for (let folder of pdfDumpFolders) {
         if (isValidPath(folder)) {
             const metadata = await getAllPDFFilesWithMedata(folder);
-            generateTextFile(metadata, pdfsOnly);
-            generateExcelOfPdfMetadata(metadata, pdfsOnly)
-            _response.push({success: true, msg: `Published book titles for ${folder}`});
+            createPdfReportAsText(metadata, pdfsOnly, path.basename(folder));
+            createExcelReport(metadata, pdfsOnly, path.basename(folder))
+            _response.push({ success: true, msg: `Published book titles for ${folder}` });
         }
         else {
             _response.push({
@@ -91,9 +97,45 @@ export const publishBookTitles = async (argFirst: string, pdfsOnly = true) => {
     };
 }
 
+function createPdfReportAsText(metadata: Array<FileStats>, pdfsOnly: boolean, folderBase: string) {
+    const report = generateTextFile(metadata, pdfsOnly);
+    const timeComponent = moment(new Date()).format(DD_MM_YYYY_HH_MMFORMAT)
+    const fileName = `${folderBase}_MegaList_${pdfsOnly ? 'pdfs_only' : 'all_files'}-${timeComponent}.txt`;
+    fs.writeFileSync(`${_root}\\${folderBase}\\${fileName}`, report);
+}
 
 function generateTextFile(metadata: Array<FileStats>, pdfsOnly: boolean) {
-    console.log(`generateTextFile-InConstruction ${metadata.length} pdfsOnly ${pdfsOnly}`)
+    console.log(`generateTextFile ${metadata.length} pdfsOnly ${pdfsOnly}`)
+    let report = '';
+    let totalFileCount = 0
+    let totalSize = 0
+    let totalPageCount = 0
+    metadata.forEach((fileStats: FileStats) => {
+        report += `${fileStats.rowCounter}). ${fileStats.fileName}, ${fileStats.pageCount} pages, ${fileStats.size}\n`
+        totalFileCount += 1;
+        totalSize += fileStats.rawSize;
+        totalPageCount = fileStats.pageCount
+    });
+
+    const summary = `PDF Stats:
+    Total File Count: ${totalFileCount}
+    Total Files Processed: ${totalFileCount}
+    Total Files with Errors(including password-protected): ?
+    Total Files system didnt pick: ?
+    Erroneous File List: ?
+    Password Protected Erroneous File List: ?
+    Total Files Read Successfully: ${totalFileCount}
+    Total Size of Files: ${sizeInfo(totalSize)}
+    Total Pages: ${totalPageCount}`
+
+    const fullReport = `${report}
+    ${summary}`
+    console.log(fullReport)
+    return fullReport;
+}
+
+function createExcelReport(metadata: Array<FileStats>, pdfsOnly: boolean, folderBase: string) {
+    generateExcelOfPdfMetadata(metadata, pdfsOnly)
 }
 
 function generateExcelOfPdfMetadata(metadata: Array<FileStats>, pdfsOnly: boolean) {
