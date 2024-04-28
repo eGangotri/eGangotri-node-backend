b2dc1eb1ad2e3171bb57c6fb4f16a62d11c6ee3a
writetochetanpandey <writetochetanpandey@gmail.com>
2024-04-28 11:57:59 +0530

    Optimizations at 28-04-2024_11-57-58.99 ='

--- src/aksharamukha/convert.ts
+++ src/aksharamukha/convert.ts
@@ -1,8 +1,11 @@
 export const callAksharamukha = async (body: AksharaMukhaGetProps) => {
     const response = await fetch(`http://aksharamukha-plugin.appspot.com/api/public?source=${body.source}&target=${body.target}&text=${body.text}&nativize=${body.nativize}`)
     const result = await response.text();
-    //console.log(result);
-    return result;
+    const e_macronRemoval = result.replace(/ē/g, 'e')
+    const n_tildaRemoval = e_macronRemoval.replace(/Jṭ/ig, 'gy')
+    
+    console.log(`For input: ${body.text} \nResult: ${n_tildaRemoval}`);
+    return n_tildaRemoval;
 }
 
 export const aksharamukhaIastToRomanColloquial = async (text: string, nativize = false) => {
@@ -22,19 +25,19 @@ export interface AksharaMukhaGetProps {
     nativize: boolean;
     postOptions?: (null)[] | null;
     preOptions?: (null)[] | null;
-  }
-  
+}
 
-const jsonBody:AksharaMukhaGetProps = {
+
+const jsonBody: AksharaMukhaGetProps = {
     "source": "IAST",
     "target": "RomanColloquial",
-    "text": "Kriyā-Prayoga $ \nTantra-Kriyā \nĀgama-Nibandha \nKośa \nĀgama-Kriyā \nKriyā-Prayoga Āgama",
+    "text": "Tamilē rayar vaittiya nul    Kriyā-Prayoga $ \nTantra-Kriyā \nĀgama-Nibandha \nKośa \nĀgama-Kriyā \nKriyā-Prayoga Āgama",
     "nativize": true,
     "postOptions": [],
     "preOptions": []
 }
-
-callAksharamukha(jsonBody);
-
+callAksharamukha(jsonBody).then((res) => {
+    console.log(res)
+})
 
 //yarn run aksharamukha
