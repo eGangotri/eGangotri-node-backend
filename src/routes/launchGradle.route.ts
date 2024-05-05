5483efc3ab30497788596c5a61e56a46745c9e89
writetochetanpandey <writetochetanpandey@gmail.com>
2024-05-05 12:13:17 +0530

    Optimizations at 05-05-2024_12-13-17.48 ='

--- src/routes/launchGradle.route.ts
+++ src/routes/launchGradle.route.ts
@@ -3,6 +3,11 @@ import { launchUploader, launchUploaderViaAbsPath, launchUploaderViaExcel, launc
 import { ArchiveProfileAndTitle } from '../mirror/types';
 import { isValidPath } from '../utils/utils';
 import { getFolderInDestRootForProfile } from '../cliBased/utils';
+import { ItemsUshered } from '../models/itemsUshered';
+import fs from 'fs';
+import moment from 'moment';
+import { DD_MM_YYYY_HH_MMFORMAT } from '../utils/constants';
+import path from 'path';
 
 export const launchGradleRoute = express.Router();
 
@@ -30,7 +35,7 @@ launchGradleRoute.get('/launchUploaderViaExcel', async (req: any, resp: any) => 
         const res = await launchUploaderViaExcel(req.query.gradleArgs)
         resp.status(200).send({
             response: {
-                success:true,
+                success: true,
                 res
             }
         });
@@ -39,8 +44,8 @@ launchGradleRoute.get('/launchUploaderViaExcel', async (req: any, resp: any) => 
     catch (err: any) {
         console.log('Error', err);
         resp.status(400).send({
-            response:{
-                success:false,
+            response: {
+                success: false,
                 err
             }
         });
@@ -55,7 +60,7 @@ launchGradleRoute.get('/launchUploaderViaJson', async (req: any, resp: any) => {
         const res = await launchUploaderViaJson(req.query.gradleArgs)
         resp.status(200).send({
             response: {
-                success:true,
+                success: true,
                 res
             }
         });
@@ -64,13 +69,59 @@ launchGradleRoute.get('/launchUploaderViaJson', async (req: any, resp: any) => {
     catch (err: any) {
         console.log('Error', err);
         resp.status(400).send({
-            response:{
-                success:false,
+            response: {
+                success: false,
                 err
             }
         });
     }
 })
+
+launchGradleRoute.get('/reuploadFailed', async (req: any, resp: any) => {
+    try {
+        const uploadCycleId = req.query.uploadCycleId
+        if (!uploadCycleId) {
+            resp.status(400).send({
+                response: {
+                    success: false,
+                    message: "uploadCycleId is required"
+                }
+            });
+            return;
+        }
+
+        console.log(`reuploadFailed ${uploadCycleId}`)
+        const _failedForUploacCycleId = await ItemsUshered.find({
+            uploadCycleId: uploadCycleId,
+            uploadFlag: false
+        });
+        const timeComponent = moment(new Date()).format(DD_MM_YYYY_HH_MMFORMAT)
+        const folder = (process.env.HOME || process.env.USERPROFILE) + path.sep + 'Downloads' + path.sep;
+        const jsonFileName = folder + `reupload-failed-${uploadCycleId}-${timeComponent}.json`;
+        console.log(`jsonFileName ${jsonFileName}`)
+        fs.writeFileSync(jsonFileName, JSON.stringify(_failedForUploacCycleId, null, 2));
+
+        const res = await launchUploaderViaJson(jsonFileName)
+        resp.status(200).send({
+            response: {
+                success: true,
+                res
+            }
+        });
+
+    }
+    catch (err: any) {
+        console.log('Error', err);
+        resp.status(400).send({
+            response: {
+                success: false,
+                err
+            }
+        });
+    }
+})
+
+
 
 launchGradleRoute.get('/launchUploaderViaAbsPath', async (req: any, resp: any) => {
     try {
@@ -85,7 +136,7 @@ launchGradleRoute.get('/launchUploaderViaAbsPath', async (req: any, resp: any) =
     catch (err: any) {
         console.log('Error', err);
         resp.status(400).send({
-            response:{
+            response: {
                 success: false,
                 message: err.message
             }
@@ -136,7 +187,7 @@ launchGradleRoute.get('/bookTitles', async (req: any, resp: any) => {
         console.log(`profileOrPaths ${profileOrPaths}`);
 
         const pdfDumpFolders = isValidPath(profileOrPaths[0]) ? profileOrPaths :
-            profileOrPaths.map((_profileOrPath:string) => {
+            profileOrPaths.map((_profileOrPath: string) => {
                 return getFolderInDestRootForProfile(_profileOrPath)
             });
 
