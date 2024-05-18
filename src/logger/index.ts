6d089322a5249dc055eeb553d68e37b7d9571987
eGangotri Digital Preservation Trust <egangotritrust@gmail.com>
2024-05-18 12:25:10 +0800

    Optimizations at Sat05-18-2024_12-25-10.02 ='

---
+++ src/logger/index.ts
@@ -0,0 +1,39 @@
+import { createLogger, format, transports } from 'winston';
+import * as os from 'os';
+import * as fs from 'fs';
+import * as fsExtra from "fs-extra";
+
+const { combine, timestamp, printf } = format;
+
+const logFormat = printf(({ level, message, timestamp }) => {
+    return `${timestamp} [${level}]: ${message}`;
+});
+
+const homeDirectory = os.homedir();
+const logsDirectory = `${homeDirectory}/egangotri/logs`;
+
+const now = new Date();
+const weekYearSuffix = getWeekOfYear(now);
+if (!fs.existsSync(logsDirectory)) {
+    fsExtra.ensureDirSync(logsDirectory);
+}
+
+const logger = createLogger({
+    level: 'info',
+    format: combine(
+        timestamp(),
+        logFormat
+    ),
+    transports: [
+        new transports.File({ filename: `${logsDirectory}/node-be-${weekYearSuffix}.log` }),
+        new transports.Console()
+    ]
+});
+
+function getWeekOfYear(date: Date): string {
+    const startOfYear = new Date(date.getFullYear(), 0, 1);
+    const weekNumber = Math.ceil((((date.getTime() - startOfYear.getTime()) / 86400000) + startOfYear.getDay() + 1) / 7);
+    return `Week-${weekNumber}-${date.getFullYear()}`;
+}
+
+export default logger;
