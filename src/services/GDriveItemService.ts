2f08060d1950599bef571e5e1cfb0ed236e9de07
Chandigarh Server <drnar@parthsharma>
2024-05-26 00:11:07 +0530

    Optimizations at 26-05-2024_0-11-07.25 ='

--- src/services/GDriveItemService.ts
+++ src/services/GDriveItemService.ts
@@ -3,7 +3,7 @@ import { replaceQuotes, replaceQuotesAndSplit } from "../excelToMongo/Util";
 import { GDriveItemListOptionsType } from "../types/listingTypes";
 import { GDriveItem } from "../models/GDriveItem";
 import { excelToJson, jsonToExcel } from "../cliBased/excel/ExcelUtils";
-import { titleInGoogleDrive } from "../cliBased/googleapi/_utils/constants";
+import { folderName, titleInGoogleDrive } from "../cliBased/googleapi/_utils/constants";
 import { DD_MM_YYYY_HH_MMFORMAT } from "../utils/constants";
 
 import moment from "moment";
@@ -78,17 +78,28 @@ export const getDiffBetweenGDriveAndLocalFiles = (gDriveExcel: string, localExce
          ${localExcelAsJSON[0]["fileName"]}`);
 
 
-  const gDriveJsonArrayTitles = gDriveExcelAsJSON.map(x => x[titleInGoogleDrive] || "");
-  const localExcelAsJSONTitles = localExcelAsJSON.map(x => x["fileName"] || "");
+  const gDriveJsonArrayTitles = gDriveExcelAsJSON.map(row => (row[folderName] + path.sep + row[titleInGoogleDrive]) || "");
+  const localExcelAsJSONTitles = localExcelAsJSON.map(row => row["absPath"] || "");
+  console.log(`gDriveJsonArrayTitles ${JSON.stringify(gDriveJsonArrayTitles[0])}`);
+  console.log(`localExcelAsJSONTitles ${JSON.stringify(localExcelAsJSONTitles[0])}`);
 
-  const diff = _.difference(localExcelAsJSONTitles, gDriveJsonArrayTitles);
+  function myCustomComparator(a: string, b: string) {
+    if (a.includes(b)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  // const diff = _.difference(localExcelAsJSONTitles, gDriveJsonArrayTitles);
+  const diff = _.differenceWith(localExcelAsJSONTitles, gDriveJsonArrayTitles, myCustomComparator);
   console.log(`diff ${diff.length} ${diff[0]}`)
 
   const _forReupload = localExcelAsJSON.filter((item) => {
-    return diff.includes(item.fileName)
+    return diff.includes(item?.absPath)
   });
 
-  console.log(`diffUplodables ${_forReupload.length} ${_forReupload[0]}`);
+  console.log(`_forReupload ${_forReupload.length} ${_forReupload[0]}`);
 
   const folder = (process.env.HOME || process.env.USERPROFILE) + path.sep + 'Downloads' + path.sep;
   const timeComponent = moment(new Date()).format(DD_MM_YYYY_HH_MMFORMAT)
@@ -99,8 +110,9 @@ export const getDiffBetweenGDriveAndLocalFiles = (gDriveExcel: string, localExce
   return {
     diffUplodables: _forReupload.length,
     excelName,
+    localGDriveDiff: `Local has: ${localExcelAsJSON.length} items. GDrive has ${gDriveExcelAsJSON.length} items. Missing ${localExcelAsJSON.length - gDriveExcelAsJSON.length}`,
     excel: `G-Drive-Local Integrity Test Excel ${excelName} created for ${_forReupload.length} missing in G-Drive`,
     msg: `${diff.length} items not found in Local`,
-    diff,
+    diff: diff.length > 100 ? "Showing first 100 only" + diff.slice(0,100) : diff,
   }
 }
