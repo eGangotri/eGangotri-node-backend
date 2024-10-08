a0754eaf4e726b29cc0620f20d359bd57b6d66f9
writetochetanpandey <writetochetanpandey@gmail.com>
2024-10-08 18:34:23 +0800

    Optimizations at Tue10-08-2024_18-34-23.49 ='

---
+++ src/routes/scanningCenter.route.ts
@@ -0,0 +1,67 @@
+import * as express from 'express';
+import { addCentersAndLibraries, getCentersAndLibraries } from '../services/scanningCenterService';
+import { ScanningCenter } from '../models/scanningCenters';
+import { validateSuperAdminUserFromRequest } from '../services/userService';
+
+export const scanningCenterRoute = express.Router();
+
+scanningCenterRoute.get('/getCenters', async (req: any, resp: any) => {
+    const centersAndLibraries = await getCentersAndLibraries();
+    console.log(`centersAndLibraries: ${JSON.stringify(centersAndLibraries)}`);
+    try {
+        resp.status(200).send({
+            response: centersAndLibraries
+        });
+    }
+
+    catch (err: any) {
+        console.log('Error:', err);
+        resp.status(200).send({
+            success: false,
+            response: `${err.message}`
+        });
+    }
+})
+
+scanningCenterRoute.post('/addCenter', async (req: any, resp: any) => {
+    try {
+        const operatorName = req.body.operatorName
+        if (await validateSuperAdminUserFromRequest(req)) {
+            const center = new ScanningCenter(req.body);
+            if (!center) {
+                return resp.status(300).send({
+                    response: {
+                        "status": "failed",
+                        "message": "Couldnt Save."
+                    }
+                });
+            }
+            const centerRes = await addCentersAndLibraries(center);
+            if (!centerRes.success) {
+                resp.status(300).send({
+                    response: centerRes
+                });
+            }
+            else {
+                console.log(`center added : ${JSON.stringify(centerRes)}`);
+                resp.status(200).send({
+                    response: centerRes
+                });
+            }
+        }
+        else {
+            resp.status(200).send({ error: `Couldn't validate User ${operatorName}` });
+        }
+    }
+
+    catch (err: any) {
+        console.log('Error:', err);
+        resp.status(200).send({
+            success: false,
+            response: `${err.message}`
+        });
+    }
+})
+
+
+
