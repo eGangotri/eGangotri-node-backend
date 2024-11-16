9c33b64af3d8411416a15185e3261fb08bcd9913
eGangotri Digital Preservation Trust <egangotritrust@gmail.com>
2024-11-16 10:29:29 +0800

    Optimizations at Sat11-16-2024_10-29-29.05 ='

--- src/services/GDriveService.ts
+++ src/services/GDriveService.ts
@@ -5,7 +5,7 @@ import { getGDriveContentsAsJson } from '../cliBased/googleapi/GoogleDriveApiRea
 import { GoogleApiData } from 'cliBased/googleapi/types';
 import { getAllFileStats } from '../utils/FileStatsUtils';
 import { PDF_EXT, ZIP_TYPE_EXT } from '../imgToPdf/utils/constants';
-import { FileStats } from 'imgToPdf/utils/types';
+import { FileStats } from '../imgToPdf/utils/types';
 
 
 export const verifyGDriveLocalIntegrity = async (_links: string[],
@@ -13,44 +13,44 @@ export const verifyGDriveLocalIntegrity = async (_links: string[],
     ignoreFolder: string, fileType: string
 ) => {
     const startTime = Date.now();
-    const _resps = [];
-    const _resps2 = [];
+    const gDriveFileStats = [];
+    const localStats = [];
 
     for (let i = 0; i < _links.length; i++) {
         console.log(`getGDriveContentsAsJson ${_links[i]} ${_folders[i]} (${fileType})`)
-        const googleDriveFileData: Array<GoogleApiData> =
+        const googleDriveFileData: GoogleApiData[] =
             await getGDriveContentsAsJson(_links[i], "", ignoreFolder, fileType);
-        _resps.push(googleDriveFileData);
+        gDriveFileStats.push(googleDriveFileData);
 
-        const localFileStats: FileStats[] = await getAllFileStats({
+        const _stats: FileStats[] = await getAllFileStats({
             directoryPath: _folders[i],
             filterExt: fileType === PDF_TYPE ? [PDF_EXT] : (fileType === ZIP_TYPE ? ZIP_TYPE_EXT : []),
             ignorePaths: [ignoreFolder],
             withLogs: true,
             withMetadata: true,
         });
-        _resps2.push(localFileStats)
+        localStats.push(_stats)
     }
     const endTime = Date.now();
     const timeTaken = endTime - startTime;
     console.log(`Time taken to retrieve google drive Listings and local file listings: ${timeInfo(timeTaken)}`);
-    const comparisonResult = compareGDriveLocalJson(_resps, _resps2);
+    const comparisonResult = compareGDriveLocalJson(gDriveFileStats, localStats);
+    console.log(`comparisonResult: ${JSON.stringify(comparisonResult)}`);
     return {
         timeTaken: timeInfo(timeTaken),
         response: {
-            googleDriveFileData: _resps,
-            fileStats: _resps2,
+            googleDriveFileData: gDriveFileStats,
+            fileStats: localStats,
             comparisonResult
         }
     }
 }
 
 export const compareGDriveLocalJson =
-    async (googleDriveFileData: Array<GoogleApiData>,
+     (googleDriveFileData: GoogleApiData[],
         localFileStats: FileStats[]) => {
-        let successCount = 0;
         let failedFiles = [];
-        const gDriveFileTotal = googleDriveFileData.length;
+        const gDriveFileTotal = googleDriveFileData?.length
         const localFileTotal = localFileStats?.length;
         if (gDriveFileTotal !== localFileTotal) {
             failedFiles.push(`Length of googleDriveFileData(${gDriveFileTotal}) and localFileStats(${localFileTotal}) are not equal`
@@ -58,12 +58,17 @@ export const compareGDriveLocalJson =
         }
 
         for (let i = 0; i < googleDriveFileData.length; i++) {
-            if (parseInt(googleDriveFileData[i].fileSizeRaw) !== localFileStats[i].rawSize) {
-                failedFiles.push(`File size mismatch: ${googleDriveFileData[i].fileName} ${localFileStats[i].fileName}`
+            const gDriveItem = googleDriveFileData[i];
+            const localItem = localFileStats.length > i ? localFileStats[i] : { fileName: "", rawSize: -1, absPath: "" };
+            console.log(`gDriveItem: ${JSON.stringify(gDriveItem)}`);
+            console.log(`localItem: ${JSON.stringify(localItem)}`);
+
+            if (parseInt(gDriveItem?.fileSizeRaw) !== localItem.rawSize) {
+                failedFiles.push(`File size mismatch: ${gDriveItem.fileName} ${localItem?.fileName}`
                 )
             }
-            if (localFileStats[i].absPath.endsWith(googleDriveFileData[i].fileName)) {
-                failedFiles.push(`File name mismatch: ${googleDriveFileData[i].fileName} ${localFileStats[i].absPath}`
+            if (localItem?.absPath?.endsWith(`${gDriveItem.parents}/${gDriveItem.fileName}`)) {
+                failedFiles.push(`File name mismatch: ${gDriveItem?.fileName} ${localItem.absPath}`
                 )
             }
         }
