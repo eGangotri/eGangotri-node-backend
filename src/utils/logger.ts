c4b0ab72e0b9a3c2e4bb31ff820052d3cc88f9d2
eGangotri Digital Preservation Trust <egangotritrust@gmail.com>
2025-04-07 03:35:46 +0530

    Optimizations at Mon04-07-2025_3-35-46.47 ='

---
+++ src/utils/logger.ts
@@ -0,0 +1,24 @@
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    ),
+  }));
+}
+
+export { logger };
