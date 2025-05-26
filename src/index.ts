6c9bb97493166078df20dd00f4d35b4bb4e83bb1
writetochetanpandey <writetochetanpandey@gmail.com>
2025-05-27 02:57:31 +0530

    Optimizations at Tue05-27-2025_2-57-31.31 ='

--- src/index.ts
+++ src/index.ts
@@ -30,12 +30,13 @@ import { gDriveRoute } from "./routes/gDrive.route";
 import { gDriveDownloadRoute } from "./routes/gDrive.download.route";
 import { pythonRoute } from "./routes/python.route";
 import { launchCmdRoute } from "./routes/launchCmd.route";
+import { ellipsis } from "./mirror/utils";
 
 const egangotri = express();
 const hostname = "localhost";
 const port = process.env.PORT || 8000;
 const args = process.argv.slice(2);
-console.log("Command-line arguments:", args);
+console.log("Command-line arguments:", ellipsis(args?.join(",")));
 const BODY_PARSER_LIMIT = '100mb';
 
 egangotri.use(express.json({limit: BODY_PARSER_LIMIT}));
