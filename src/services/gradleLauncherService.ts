4c9a5aec0deb4ef21e89401388ccd196aba0ccb0
eGangotri Digital Preservation Trust <egangotritrust@gmail.com>
2026-08-13 10:51:20 +0530

    Optimizations at Thu08-13-2026_10-51-20.67 ='

--- src/services/gradleLauncherService.ts
+++ src/services/gradleLauncherService.ts
@@ -123,7 +123,9 @@ export function launchUploaderViaAbsPath(args: any): Promise<Record<string, any>
 
 export function reuploadMissed(itemsForReupload: ArchiveProfileAbsPathAndUploadCycleId[]): Promise<Record<string, any>> {
     console.log(`reuploadMissed ${JSON.stringify(itemsForReupload)}`);
-    const dataAsCSV = itemsForReupload.map((x: ArchiveProfileAbsPathAndUploadCycleId) => x.archiveProfile + ", '" + x.absolutePath?.trim() + (x?.uploadCycleId ? (", '" + x?.uploadCycleId?.trim()) : "") + "'").join(" ")
+    const dataAsCSV = itemsForReupload.map((x: ArchiveProfileAbsPathAndUploadCycleId) => {
+        return x.archiveProfile + ", '" + x.absolutePath?.trim() + "'" + (x?.uploadCycleId ? (", '" + x?.uploadCycleId?.trim() + "'") : "");
+    }).join(" ");
     return makeGradleCall(generateGradleCommand(dataAsCSV, "uploadToArchiveSelective"))
 }
 
